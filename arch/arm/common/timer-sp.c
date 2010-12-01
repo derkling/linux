0d5296068538029f1313ca21a4c251983ef20b22
Liviu Dudau <Liviu.Dudau@arm.com>
2010-12-01 13:30:41 +0000

    ARM: Allow for more than 2 timers to be enabled by platform code.
    The SP804 timer code has static structures for only two timers to use (one
    as a free-running source and one as a timer event). Most platforms however
    have at least 4 timers available, with the remaining two being unused due
    to the static structure restrictions.
    
    This change allows for dynamic allocation of timer structures. Note that
    the clock framework has no method for removing the timers added from the
    timers list, so timers that can appear and dissapear from the memory space
    should not use this code.

--- arch/arm/common/timer-sp.c
+++ arch/arm/common/timer-sp.c
@@ -25,8 +25,22 @@
 #include <linux/interrupt.h>
 #include <linux/irq.h>
 #include <linux/io.h>
+#include <linux/slab.h>
 
 #include <asm/hardware/arm_timer.h>
+
+struct sp804_clocksource {
+	struct clocksource	cs;
+	void __iomem		*base;
+};
+
+struct sp804_clock_event_device {
+	struct clock_event_device dev;
+	void __iomem		*base;
+};
+
+#define to_sp804_clocksource(x) container_of((x), struct sp804_clocksource, cs)
+#define to_sp804_clock_event_device(x) container_of((x), struct sp804_clock_event_device, dev);
 
 /*
  * These timers are currently always setup to be clocked at 1MHz.
@@ -34,72 +48,81 @@
 #define TIMER_FREQ_HZ	(1000000)
 
 static unsigned long sp804_clksrc_rate = TIMER_FREQ_HZ;
-static void __iomem *clksrc_base;
 
 static cycle_t sp804_read(struct clocksource *cs)
 {
-	return ~readl(clksrc_base + TIMER_VALUE);
+	struct sp804_clocksource* sp804_cs = to_sp804_clocksource(cs);
+	return ~readl(sp804_cs->base + TIMER_VALUE);
 }
-
-static struct clocksource clocksource_sp804 = {
-	.rating		= 200,
-	.read		= sp804_read,
-	.mask		= CLOCKSOURCE_MASK(32),
-	.flags		= CLOCK_SOURCE_IS_CONTINUOUS,
-};
 
 void __init sp804_clocksource_init(void __iomem *base, char *clk_id)
 {
 	struct clk *clk;
-	struct clocksource *cs = &clocksource_sp804;
+	struct sp804_clocksource *cs = kzalloc(sizeof(struct sp804_clocksource), GFP_KERNEL);
 
-	cs->name = clk_id ? clk_id : "timer-sp";
+	if (!cs)
+		return;
 
-	clksrc_base = base;
+	cs->cs.name = clk_id ? clk_id : "timer-sp";
+	cs->cs.rating = 200;
+	cs->cs.read = sp804_read;
+	cs->cs.mask = CLOCKSOURCE_MASK(32);
+	cs->cs.shift = 20;
+	cs->cs.flags = CLOCK_SOURCE_IS_CONTINUOUS;
+	cs->base = base;
 
 	clk = clk_get_sys("sp804", clk_id);
-	if (!IS_ERR(clk))
+	if (!IS_ERR(clk)) {
 		sp804_clksrc_rate = clk_get_rate(clk);
+	}
+	printk(KERN_INFO "SP804: initialising %s clock with rate %ld\n", cs->cs.name, sp804_clksrc_rate);
 
 	/* setup timer 0 as free-running clocksource */
-	writel(0, clksrc_base + TIMER_CTRL);
-	writel(0xffffffff, clksrc_base + TIMER_LOAD);
-	writel(0xffffffff, clksrc_base + TIMER_VALUE);
+	writel(0, base + TIMER_CTRL);
+	writel(0xffffffff, base + TIMER_LOAD);
+	writel(0xffffffff, base + TIMER_VALUE);
 	writel(TIMER_CTRL_32BIT | TIMER_CTRL_ENABLE | TIMER_CTRL_PERIODIC,
-		clksrc_base + TIMER_CTRL);
+		base + TIMER_CTRL);
 
-	clocksource_register_khz(cs, sp804_clksrc_rate / 1000);
+	clocksource_register_khz(&cs->cs, sp804_clksrc_rate / 1000);
 }
 
 
 static unsigned long sp804_clkevt_rate = TIMER_FREQ_HZ;
-static void __iomem *clkevt_base;
 
 /*
  * IRQ handler for the timer
  */
 static irqreturn_t sp804_timer_interrupt(int irq, void *dev_id)
 {
+	unsigned long sp804mis;
 	struct clock_event_device *evt = dev_id;
+	struct sp804_clock_event_device *sp804_evt = to_sp804_clock_event_device(evt);
 
-	/* clear the interrupt */
-	writel(1, clkevt_base + TIMER_INTCLR);
+	sp804mis = readl(sp804_evt->base + TIMER_MIS);
 
-	evt->event_handler(evt);
+	if (sp804mis) {
+		/* clear the interrupt */
+		writel(1, sp804_evt->base + TIMER_INTCLR);
 
-	return IRQ_HANDLED;
+		evt->event_handler(evt);
+		return IRQ_HANDLED;
+	}
+
+	return IRQ_NONE;
 }
 
 static void sp804_set_mode(enum clock_event_mode mode,
 	struct clock_event_device *evt)
 {
 	unsigned long ctrl = TIMER_CTRL_32BIT | TIMER_CTRL_IE;
+	struct sp804_clock_event_device *sp804_evt = to_sp804_clock_event_device(evt);
 
-	writel(ctrl, clkevt_base + TIMER_CTRL);
+	writel(ctrl, sp804_evt->base + TIMER_CTRL);
 
 	switch (mode) {
 	case CLOCK_EVT_MODE_PERIODIC:
-		writel(sp804_clkevt_rate / HZ, clkevt_base + TIMER_LOAD);
+		writel(sp804_clkevt_rate / HZ, sp804_evt->base + TIMER_LOAD);
 		ctrl |= TIMER_CTRL_PERIODIC | TIMER_CTRL_ENABLE;
 		break;
 
@@ -114,55 +137,62 @@ static void sp804_set_mode(enum clock_event_mode mode,
 		break;
 	}
 
-	writel(ctrl, clkevt_base + TIMER_CTRL);
+	writel(ctrl, sp804_evt->base + TIMER_CTRL);
 }
 
 static int sp804_set_next_event(unsigned long next,
 	struct clock_event_device *evt)
 {
-	unsigned long ctrl = readl(clkevt_base + TIMER_CTRL);
+	unsigned long ctrl;
+	struct sp804_clock_event_device *sp804_evt = to_sp804_clock_event_device(evt);
 
-	writel(next, clkevt_base + TIMER_LOAD);
-	writel(ctrl | TIMER_CTRL_ENABLE, clkevt_base + TIMER_CTRL);
+	ctrl = readl(sp804_evt->base + TIMER_CTRL);
+	writel(next, sp804_evt->base + TIMER_LOAD);
+	writel(ctrl | TIMER_CTRL_ENABLE, sp804_evt->base + TIMER_CTRL);
 
 	return 0;
 }
-
-static struct clock_event_device sp804_clockevent = {
-	.shift		= 32,
-	.features       = CLOCK_EVT_FEAT_PERIODIC | CLOCK_EVT_FEAT_ONESHOT,
-	.set_mode	= sp804_set_mode,
-	.set_next_event	= sp804_set_next_event,
-	.rating		= 300,
-	.cpumask	= cpu_all_mask,
-};
-
-static struct irqaction sp804_timer_irq = {
-	.name		= "timer",
-	.flags		= IRQF_DISABLED | IRQF_TIMER | IRQF_IRQPOLL,
-	.handler	= sp804_timer_interrupt,
-	.dev_id		= &sp804_clockevent,
-};
 
 void __init sp804_clockevents_init(void __iomem *base, unsigned int timer_irq,
 				   char *clk_id)
 {
 	struct clk *clk;
-	struct clock_event_device *evt = &sp804_clockevent;
+	struct sp804_clock_event_device *evt;
+	struct irqaction *sp804_timer_irq;
 
-	evt->name = clk_id ? clk_id : "timer-sp";
+	evt = kzalloc(sizeof(struct sp804_clock_event_device), GFP_KERNEL);
+	if (!evt)
+		return;
 
-	clkevt_base = base;
+	sp804_timer_irq = kzalloc(sizeof(struct irqaction), GFP_KERNEL);
+	if (!sp804_timer_irq) {
+		kfree(evt);
+		return;
+	}
+
+	evt->dev.name = clk_id ? clk_id : "timer-sp";
+	evt->dev.shift = 32;
+	evt->dev.features = CLOCK_EVT_FEAT_PERIODIC | CLOCK_EVT_FEAT_ONESHOT;
+	evt->dev.set_mode = sp804_set_mode;
+	evt->dev.set_next_event = sp804_set_next_event;
+	evt->dev.rating = 300;
+	evt->dev.cpumask = cpu_all_mask;
+	evt->base = base;
 
 	clk = clk_get_sys("sp804", clk_id);
 	if (!IS_ERR(clk))
 		sp804_clkevt_rate = clk_get_rate(clk);
 
-	evt->irq = timer_irq;
-	evt->mult = div_sc(sp804_clkevt_rate / 1000, NSEC_PER_MSEC, evt->shift);
-	evt->max_delta_ns = clockevent_delta2ns(0xffffffff, evt);
-	evt->min_delta_ns = clockevent_delta2ns(0xf, evt);
+	evt->dev.irq = timer_irq;
+	evt->dev.mult = div_sc(sp804_clkevt_rate / 1000, NSEC_PER_MSEC, evt->dev.shift);
+	evt->dev.max_delta_ns = clockevent_delta2ns(0xffffffff, &evt->dev);
+	evt->dev.min_delta_ns = clockevent_delta2ns(0xf, &evt->dev);
 
-	setup_irq(timer_irq, &sp804_timer_irq);
-	clockevents_register_device(evt);
+	sp804_timer_irq->name = "timer";
+	sp804_timer_irq->flags = IRQF_TIMER | IRQF_IRQPOLL | IRQF_SHARED | IRQF_PROBE_SHARED;
+	sp804_timer_irq->handler = sp804_timer_interrupt;
+	sp804_timer_irq->dev_id = evt;
+
+	setup_irq(timer_irq, sp804_timer_irq);
+	clockevents_register_device(&evt->dev);
 }
