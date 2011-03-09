9a5d98a07ca5cd0185de5ea308b44d1a9ecbc003
Liviu Dudau <Liviu.Dudau@arm.com>
2011-03-09 15:57:33 +0000

    ARM: VExpress - Tuscan: Fix the use of debug serial port when the daughterboard UART is not enabled.
    Missed a #ifdef to test for the activation of the daughterboard UART when
    enabling the debug serial port.

--- arch/arm/mach-vexpress/include/mach/uart.h
+++ arch/arm/mach-vexpress/include/mach/uart.h
@@ -3,10 +3,13 @@
 
 #include <mach/motherboard.h>
 
+#ifdef CONFIG_VEXPRESS_USE_TILE_UART0
+
 #ifdef CONFIG_ARCH_VEXPRESS_LT_ELBA
 #include <mach/lt-elba.h>
-
 #define VEXPRESS_UART0		LT_ELBA_UART0
+#endif
+
 #else
 #define VEXPRESS_UART0		V2M_UART0
 #endif
