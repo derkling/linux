15884b35a3d89972471baa85c0babe0594de0884
Richard Cochran <richardcochran@gmail.com>
2011-04-05 18:10:42 +0100

    [PATCH] arm: disable non-existent prpmc1100 code.
    Commit 6f82f4db80189281a8ac42f2e72396accb719b57 removed the prpmc110 from
    the list of machine types. This commit disables a PCI fixup function for
    that board, since its presence is preventing the kernel from compiling.
    
    Signed-off-by: Richard Cochran <richard.cochran@omicron.at>

--- arch/arm/kernel/bios32.c
+++ arch/arm/kernel/bios32.c
@@ -167,6 +167,7 @@ DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_DEC, PCI_DEVICE_ID_DEC_21285, pci_fixup_d
  * with an IXP4xx target device and we don't want to kill the
  * resources in that case.
  */
+#ifdef CONFIG_ARCH_PRPMC1100
 static void __devinit pci_fixup_prpmc1100(struct pci_dev *dev)
 {
 	int i;
@@ -182,6 +183,7 @@ static void __devinit pci_fixup_prpmc1100(struct pci_dev *dev)
 	}
 }
 DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_IXP4XX, pci_fixup_prpmc1100);
+#endif
 
 /*
  * PCI IDE controllers use non-standard I/O port decoding, respect it.
