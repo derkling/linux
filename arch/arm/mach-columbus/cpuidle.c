5eab9d88a94887fa7f14f00fa2a3a59a6d601708
Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
2012-03-08 18:03:26 +0000

    ARM: columbus: CPU idle baseline implementation
    This patch defines the skeleton driver for the complete Columbus
    CPU idle implementation. All CPU states are supported inclusive of
    cluster states.
    
    In the Columbus platform the kernel runs in non-secure mode so that all
    power down requests coming from OSPM subsystems have to be executed through
    an SMC interface.
    
    For that purpose, the patch also defines an assembly stub implementing the
    CPU shutdown SMC call defined in the OSPM design document.
    
    The driver keeps a cpumask of CPUs in deep shutdown so that the driver
    is capable of eliciting the proper cluster state before issuing the
    shutdown SMC call.
    
    Idle code is heavily based on common save/restore, in particular the
    driver relies on CPU PM notifiers to save/restore the state of VFP and
    GIC, and calls into cpu_suspend in order to save the CPU context.
    
    Currently, C-states are defined per-driver so that it is impossible for
    the driver to cater for the C-state table differences that come up when
    every CPU can be switched to a CPU in another cluster behind kernel
    control.

---
+++ arch/arm/mach-columbus/cpuidle.c
@@ -0,0 +1,303 @@
+/*
+ * Columbus CPU idle driver.
+ *
+ * Copyright (C) 2012 ARM Ltd.
+ * Author: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/cpuidle.h>
+#include <linux/cpumask.h>
+#include <linux/cpu_pm.h>
+#include <linux/clockchips.h>
+#include <linux/hrtimer.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/tick.h>
+#include <trace/events/power.h>
+#include <asm/smp_plat.h>
+#include <asm/suspend.h>
+
+#define COLUMBUS_STATE_C1 0
+#define COLUMBUS_STATE_C2 1
+#define COLUMBUS_STATE_C3 2
+#define COLUMBUS_STATE_C4 3
+#define COLUMBUS_MAX_STATES (COLUMBUS_STATE_C4 + 1)
+
+/* four bits reserved to cpu state */
+#define CPU_ST(x) (x & 0xf)
+#define CLUSTER_ST(x) (x >> 4 & 0xf)
+#define CSS_STATE(cpuid, clusterid, cpustate, clusterstate) \
+	((clusterstate) << 12 | (cpustate) << 8 | (clusterid) << 4 | (cpuid))
+
+struct columbus_processor_cx {
+	u8 valid;
+	u8 type;
+	u8 cr;
+	u32 sleep_latency;
+	u32 wakeup_latency;
+	u32 threshold;
+	u32 flags;
+};
+
+struct columbus_processor_cx columbus_power_states[COLUMBUS_MAX_STATES] = {
+	{
+		.valid = 1,
+		.type = COLUMBUS_STATE_C1,
+		.sleep_latency = 2,
+		.wakeup_latency = 2,
+		.threshold = 1,
+		.cr = 0x1, /* wfi */
+		.flags = CPUIDLE_FLAG_TIME_VALID,
+	},
+	{
+		.valid = 1,
+		.type = COLUMBUS_STATE_C2,
+		.sleep_latency = 30,
+		.wakeup_latency = 30,
+		.threshold = 50,
+		.cr = 0x3, /* CPU shut down cluster on - clocked */
+		.flags = CPUIDLE_FLAG_TIME_VALID,
+	},
+	{
+		.valid = 1,
+		.type = COLUMBUS_STATE_C3,
+		.sleep_latency = 50,
+		.wakeup_latency = 50,
+		.threshold = 300,
+		.cr = 0x23, /* shut down cluster off - L2 retention */
+		.flags = CPUIDLE_FLAG_TIME_VALID,
+	},
+	{
+		.valid = 1,
+		.type = COLUMBUS_STATE_C4,
+		.sleep_latency = 1500,
+		.wakeup_latency = 1800,
+		.threshold = 4000,
+		.cr = 0x33, /* complete cluster shutdown */
+		.flags = CPUIDLE_FLAG_TIME_VALID,
+	},
+};
+
+struct cpuidle_driver columbus_idle_driver = {
+	.name = "columbus_idle",
+	.owner = THIS_MODULE,
+	.state_count = COLUMBUS_MAX_STATES,
+	.safe_state_index = 0
+};
+
+static DEFINE_SPINLOCK(cluster_lock);
+static DEFINE_PER_CPU(struct cpuidle_device, columbus_idle_dev);
+
+static cpumask_t idle_mask = { CPU_BITS_NONE };
+
+extern int shutdown_smc(u32, u32, u32, u32);
+
+static inline int arch_wakeup_ts(u64 *ts)
+{
+	u32 cvall, cvalh;
+	s32 val;
+
+	asm volatile("mrc p15, 0, %0, c14, c2, 0" : "=r" (val));
+
+	asm volatile("mrrc p15, 0, %0, %1, c14" : "=r" (cvall), "=r" (cvalh));
+
+	if (val < 0)
+		return 1;
+
+	*ts = (val + (u64) (((u64) cvalh << 32) | cvall));
+
+	return 0;
+}
+union wakeup {
+	struct {
+		u32 wakeup_l;
+		u32 wakeup_h;
+	} ts32;
+	u64 ts64;
+};
+
+/*
+ * Power down SCP command passed as an argument from cpu_suspend
+ */
+int columbus_finisher(unsigned long arg)
+{
+	int ret;
+	union wakeup w;
+
+	if (arch_wakeup_ts(&w.ts64))
+		return 1;
+
+	ret = shutdown_smc(0, arg, w.ts32.wakeup_h, w.ts32.wakeup_l);
+	return ret;
+}
+
+/*
+ * columbus_enter_idle - Programs CPU to enter the specified state
+ * @dev: cpuidle device
+ * @drv: The target state to be programmed
+ * @idx: state index
+ *
+ * Called from the CPUidle framework to program the device to the
+ * specified target state selected by the governor.
+ */
+static int columbus_enter_idle(struct cpuidle_device *dev,
+				struct cpuidle_driver *drv, int idx)
+{
+	struct columbus_processor_cx *cx =
+		cpuidle_get_statedata(&dev->states_usage[idx]);
+
+	struct tick_device *tdev = tick_get_device(dev->cpu);
+	struct timespec ts_preidle, ts_postidle, ts_idle;
+	int ret, cpu_id, cluster_state, cluster_down = 0;
+	u32 mpidr, command;
+
+	/* Used to keep track of the total time in idle */
+	getnstimeofday(&ts_preidle);
+
+	if (cx->type == COLUMBUS_STATE_C1) {
+
+		/* Used to keep track of the total time in idle */
+		asm volatile("wfi");
+		getnstimeofday(&ts_postidle);
+		ts_idle = timespec_sub(ts_postidle, ts_preidle);
+
+		local_irq_enable();
+		local_fiq_enable();
+
+		dev->last_residency = ts_idle.tv_nsec / NSEC_PER_USEC +
+						ts_idle.tv_sec * USEC_PER_SEC;
+		return 0;
+	}
+
+	spin_lock(&cluster_lock);
+
+	cpu_pm_enter();
+
+	if (cx->type > COLUMBUS_STATE_C2)
+		__cpu_set(dev->cpu, &idle_mask);
+
+	cluster_down = (cpumask_weight(&idle_mask) == num_online_cpus());
+
+	if (cluster_down) {
+		cpu_cluster_pm_enter();
+		for_each_cpu(cpu_id, cpu_online_mask) {
+			if (cx->type > COLUMBUS_STATE_C2) {
+				trace_power_start(POWER_CSTATE, cx->type + 2,
+					cpu_id);
+				trace_cpu_idle(cx->type + 2, cpu_id);
+			}
+		}
+	}
+
+	mpidr = cpu_logical_map(dev->cpu);
+	cluster_state = cluster_down ? CLUSTER_ST(cx->cr) : 0;
+	command = CSS_STATE(mpidr & 0xff,
+				(mpidr & 0xff00) >> 8,
+				CPU_ST(cx->cr),
+				cluster_state);
+	spin_unlock(&cluster_lock);
+
+	ret = cpu_suspend(command, columbus_finisher);
+
+	spin_lock(&cluster_lock);
+
+	if (cluster_down) {
+		for_each_cpu(cpu_id, cpu_online_mask) {
+			if (cx->type > COLUMBUS_STATE_C2) {
+				trace_power_end(cpu_id);
+				trace_cpu_idle(PWR_EVENT_EXIT, cpu_id);
+			}
+		}
+	}
+
+	if (ret)
+		goto out;
+
+	if (cluster_down)
+		cpu_cluster_pm_exit();
+
+	cpu_pm_exit();
+
+	/* Restore the per-cpu timer event */
+	clockevents_program_event(tdev->evtdev, tdev->evtdev->next_event, 1);
+out:
+
+	if (cx->type > COLUMBUS_STATE_C2)
+		__cpu_clear(dev->cpu, &idle_mask);
+
+	cluster_down = 0;
+
+	spin_unlock(&cluster_lock);
+
+	getnstimeofday(&ts_postidle);
+	ts_idle = timespec_sub(ts_postidle, ts_preidle);
+
+	dev->last_residency = ts_idle.tv_nsec / NSEC_PER_USEC +
+					ts_idle.tv_sec * USEC_PER_SEC;
+	local_irq_enable();
+	return idx;
+}
+
+/*
+ * columbus_idle_init
+ *
+ * Registers the Columbus specific cpuidle driver with the cpuidle
+ * framework with the valid set of states.
+ */
+int __init columbus_idle_init(void)
+{
+	struct columbus_processor_cx *cx;
+	struct cpuidle_state *state;
+	struct cpuidle_device *dev;
+	int i, cpu_id, count;
+
+	cpuidle_register_driver(&columbus_idle_driver);
+
+	for (count = 0, i = COLUMBUS_STATE_C1; i < COLUMBUS_MAX_STATES; i++) {
+		cx = &columbus_power_states[i];
+		state = &columbus_idle_driver.states[count];
+
+		if (!cx->valid)
+			continue;
+
+		state->exit_latency = cx->sleep_latency +
+						cx->wakeup_latency;
+		state->target_residency = cx->threshold;
+		state->flags = cx->flags;
+		state->enter = columbus_enter_idle;
+		sprintf(state->name, "C%d", count + 1);
+		count++;
+	}
+
+	for_each_cpu(cpu_id, cpu_online_mask) {
+		pr_err("CPUidle for CPU%d registered\n", cpu_id);
+		dev = &per_cpu(columbus_idle_dev, cpu_id);
+		dev->cpu = cpu_id;
+
+		dev->state_count = COLUMBUS_MAX_STATES;
+
+		for (count = 0, i = COLUMBUS_STATE_C1;
+				i < COLUMBUS_MAX_STATES; i++) {
+			cx = &columbus_power_states[i];
+
+			if (!cx->valid)
+				continue;
+			cpuidle_set_statedata(&dev->states_usage[count], cx);
+			count++;
+		}
+
+		if (cpuidle_register_device(dev)) {
+			printk(KERN_ERR "%s: Cpuidle register device failed\n",
+			       __func__);
+			return -EIO;
+		}
+	}
+
+
+	return 0;
+}
+device_initcall(columbus_idle_init);
