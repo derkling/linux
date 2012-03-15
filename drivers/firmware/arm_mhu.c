d3542298516939566043970e09f9fd06b8405c1d
Giuseppe Calderaro <giuseppe.calderaro@arm.com>
2012-03-15 13:47:22 +0000

    ARM: Completed MHU driver

--- drivers/firmware/arm_mhu.c
+++ drivers/firmware/arm_mhu.c
@@ -1,3 +1,4 @@
+#include <linux/completion.h>
 #include <linux/interrupt.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
@@ -5,35 +6,266 @@
 #include <asm/io.h>
 #include "arm_mhu.h"
 
-int get_dvfs_size(int cluster, int cpu, int *size)
+static struct arm_mhu_data *gdata;
+static struct list_head lo_head;
+static struct mutex lo_mutex;
+static struct list_head hi_head;
+static struct mutex hi_mutex;
+
+static inline void init_req(struct arm_mhu_request *req, u32 cmd)
 {
-	return 0;
+	req->cmd = cmd;
+	init_completion(&req->sync);
 }
 
-int get_dvfs_capabilities(int cluster, int cpu, int *freqs)
+static inline void append_req(struct arm_mhu_request *req, struct mutex *mutex, struct list_head *head)
 {
-	return 0;
+	mutex_lock(mutex);
+	list_add_tail(&req->list, head);
+	mutex_unlock(mutex);
 }
 
-int get_performance(int cluster, int cpu, int *perf)
+static inline void wait_for_mhu(void)
 {
-	return 0;
+	volatile u32 status;
+
+	do {
+		status = mhu_reg_readl(gdata, SCP_INTR_L_STAT);
+		cpu_relax();
+	} while (status);	          
 }
 
-int set_performance(int cluster, int cpu, int index)
+int get_dvfs_size(int cluster, int cpu, u32 *size)
 {
+	struct arm_mhu_request *req;
+
+	if (!size)
+		return -EFAULT;
+
+	req = kzalloc(sizeof(*req), GFP_KERNEL);
+	if (!req)
+		return -ENOMEM;
+
+	init_req(req, GET_CAPABILITIES);
+	append_req(req, &lo_mutex, &lo_head);
 	
+	/* Check if SPC can deal with us.  */
+	wait_for_mhu();
+
+	/* Fill payload.  */
+	mhu_mem_writel(gdata, SCP_LOW, 1);
+	/* Write set register.  */
+	mhu_reg_writel(gdata, SCP_INTR_L_SET, 
+		       (1 << 20) | 
+		       (0 /* flags  */ << 16) | 
+		       (cluster << 12) | 
+		       (cpu << 8) |
+		       GET_CAPABILITIES);
+
+	/* Wait for response.  */
+	wait_for_completion(&req->sync);
+
+	*size = req->payload_size;
+
+	kfree(req);
 
 	return 0;
+}
+
+int get_dvfs_capabilities(int cluster, int cpu, u32 *freqs, u32 size)
+{
+	struct arm_mhu_request *req;
+	int i;
+
+	if (!freqs)
+		return -EFAULT;
+
+	req = kzalloc(sizeof(*req), GFP_KERNEL);
+	if (!req)
+		return -ENOMEM;
+
+	init_req(req, GET_CAPABILITIES);
+	append_req(req, &lo_mutex, &lo_head);
+	
+	/* Check if SPC can deal with us.  */
+	wait_for_mhu();
+
+	/* Fill payload.  */
+	mhu_mem_writel(gdata, SCP_LOW, 1);
+	/* Write set register.  */
+	mhu_reg_writel(gdata, SCP_INTR_L_SET, 
+		       (1 << 20) | 
+		       (0 /* flags  */ << 16) | 
+		       (cluster << 12) | 
+		       (cpu << 8) |
+		       GET_CAPABILITIES);
+
+	/* Wait for response.  */
+	wait_for_completion(&req->sync);
+
+	for (i = 0; i < size; i++) {
+		freqs[i] = req->payload[i];
+	}
+
+	kfree(req->payload);
+	kfree(req);
+
+	return 0;
+}
+
+int get_performance(int cluster, int cpu, u32 *perf)
+{
+	struct arm_mhu_request *req;
+
+	if (!perf)
+		return -EFAULT;
+
+	req = kzalloc(sizeof(*req), GFP_KERNEL);
+	if (!req)
+		return -ENOMEM;
+
+	init_req(req, GET_PERFORMANCE);
+	append_req(req, &lo_mutex, &lo_head);
+	      
+	/* Check if SPC can deal with us.  */
+	wait_for_mhu();
+
+	/* Fill payload.  */
+	mhu_mem_writel(gdata, SCP_LOW, cluster & 0xFF);
+	wmb();
+	mhu_reg_writel(gdata, SCP_INTR_L_SET, (1 << 20) | 
+		       (0 /* flags  */ << 16) | 
+		       (cluster << 12) | 
+		       (cpu << 8) | 
+		       GET_PERFORMANCE);
+
+	/* Wait for response.  */
+	wait_for_completion(&req->sync);
+
+	*perf = req->payload[0];
+	if (*perf & 0xFF) {
+		printk(KERN_ERR "MHU Cmd failed\n");
+		return -EFAULT;
+	}
+	*perf = (*perf >> 16) & 0xFF;
+
+	kfree(req->payload);
+	kfree(req);
+
+	return 0;
+}
+
+int set_performance(int cluster, int cpu, u32 index)
+{
+	struct arm_mhu_request *req;
+	int ret;
+
+	req = kzalloc(sizeof(*req), GFP_KERNEL);
+	if (!req)
+		return -ENOMEM;
+
+	init_req(req, SET_PERFORMANCE);
+	append_req(req, &hi_mutex, &hi_head);
+	      
+	/* Check if SPC can deal with us.  */
+	wait_for_mhu();
+
+	/* Fill payload.  */
+	mhu_mem_writel(gdata, SCP_HIGH, (cluster & 0xFF) | ((index << 8) && 0xFF00));
+	wmb();
+	mhu_reg_writel(gdata, SCP_INTR_H_SET, (2 << 20) | 
+		       (0 /* flags  */ << 16) | 
+		       (cluster << 12) | 
+		       (cpu << 8) | 
+		       SET_PERFORMANCE);
+
+	/* Wait for response.  */
+	wait_for_completion(&req->sync);
+
+	if (req->payload[0] && 0xFF)
+		ret = req->payload[0] & 0xFF;
+	else
+		ret = 0;
+
+	kfree(req->payload);
+	kfree(req);	
+
+	return ret;
 }
 
 static irqreturn_t arm_mhu_hi_irq_handler(int irq, void *dev_id)
 {
+	struct arm_mhu_data *data = dev_id;
+	u32 status = mhu_reg_readl(dev_id, SCP_INTR_H_STAT);	
+	u32 cmd = status & 0xFF;
+	struct list_head *pos;
+	struct arm_mhu_request *req = 0;
+	int i;
+
+	mutex_lock(&hi_mutex);
+	list_for_each(pos, &hi_head) {
+		req = list_entry(pos, struct arm_mhu_request, list);
+		if (req->cmd == cmd) {
+			list_del(&req->list);
+			break;
+		}
+	}
+	mutex_unlock(&hi_mutex);	
+
+	req->payload_size = (status & 0x7ff00000) >> 20;
+	req->payload = kzalloc(sizeof(u8) * req->payload_size, GFP_KERNEL);
+	if (!req->payload) {
+		printk(KERN_ERR "MHU: can't allocate memory for payload.\n");
+		goto out;
+	}
+
+	for (i = 0; i < req->payload_size; i++)
+		req->payload[i] = mhu_mem_readl(gdata, SCP_LOW + 4 * i);
+
+	/* Clear SPC interrupt.  */
+	mhu_reg_writel(data, SCP_INTR_H_CLEAR, status);
+
+	complete(&req->sync);
+
+out:
 	return IRQ_HANDLED;
 }
 
 static irqreturn_t arm_mhu_lo_irq_handler(int irq, void *dev_id)
 {
+	struct arm_mhu_data *data = dev_id;
+	u32 status = mhu_reg_readl(dev_id, SCP_INTR_L_STAT);	
+	u32 cmd = status & 0xFF;
+	struct list_head *pos;
+	struct arm_mhu_request *req = 0;
+	int i;
+
+	mutex_lock(&lo_mutex);
+	list_for_each(pos, &lo_head) {
+		req = list_entry(pos, struct arm_mhu_request, list);
+		if (req->cmd == cmd) {
+			list_del(&req->list);
+			break;
+		}
+	}
+	mutex_unlock(&lo_mutex);	
+
+	req->payload_size = (status & 0x7ff00000) >> 20;
+	req->payload = kzalloc(sizeof(u32) * req->payload_size, GFP_KERNEL);
+	if (!req->payload) {
+		printk(KERN_ERR "MHU: can't allocate memory for payload.\n");
+		goto out;
+	}
+
+	for (i = 0; i < req->payload_size; i++)
+		req->payload[i] = mhu_mem_readl(gdata, SCP_LOW + 4 * i);
+
+	/* Clear SPC interrupt.  */
+	mhu_reg_writel(data, SCP_INTR_L_CLEAR, status);
+
+	complete(&req->sync);
+
+out:
 	return IRQ_HANDLED;
 }
 
@@ -43,7 +275,7 @@ static __devinit int arm_mhu_probe(struct platform_device *pdev)
 	struct resource *res;
 	int ret = 0;
 
-	data= kzalloc(sizeof(*data), GFP_KERNEL);
+	data = kzalloc(sizeof(*data), GFP_KERNEL);
 	if (!data) {
 		dev_err(&pdev->dev, "Unable to allocate private data\n");
 		return -ENOMEM;
@@ -93,14 +325,14 @@ static __devinit int arm_mhu_probe(struct platform_device *pdev)
 	}
 	data->lo_irq = res->start;     
 
-	ret = request_irq(data->hi_irq, arm_mhu_hi_irq_handler, 0, "arm_mhu_hi_irq", data);
+	ret = request_threaded_irq(data->hi_irq, 0, arm_mhu_hi_irq_handler, 0, "arm_mhu_hi_irq", data);
 	if (ret) {
 		dev_err(&pdev->dev, "hi priority irq request failed\n");
 		ret = -ENODEV;
 		goto remap_free;
 	}
 
-	ret = request_irq(data->lo_irq, arm_mhu_lo_irq_handler, 0, "arm_mhu_hi_irq", data);
+	ret = request_threaded_irq(data->lo_irq, 0, arm_mhu_lo_irq_handler, 0, "arm_mhu_hi_irq", data);
 	if (ret) {
 		dev_err(&pdev->dev, "lo priority irq request failed\n");
 		ret = -ENODEV;
@@ -108,6 +340,12 @@ static __devinit int arm_mhu_probe(struct platform_device *pdev)
 	}
 
 	platform_set_drvdata(pdev, data);
+
+	mutex_init(&lo_mutex);
+	mutex_init(&hi_mutex);
+	INIT_LIST_HEAD(&lo_head);
+	INIT_LIST_HEAD(&hi_head);
+	gdata = data;
 
 out:
 	return ret;
--- drivers/firmware/arm_mhu.h
+++ drivers/firmware/arm_mhu.h
@@ -1,6 +1,8 @@
 #ifndef _ARM_MHU_H
 #define _ARM_MHU_H
 
+#include <linux/types.h>
+
 /* Registers.  */
 #define SCP_INTR_L_STAT  0x000
 #define SCP_INTR_L_SET   0x008
@@ -30,27 +32,68 @@
 #define SCP_HIGH     0x400
 #define CPU_HIGH     0x600
 
+/* Commands list.  */
+#define GET_CAPABILITIES 1
+#define SET_PERFORMANCE  6
+#define GET_PERFORMANCE  7
+
 struct arm_mhu_data {
 	void *regs;
 	void *mem;
 	int hi_irq;
 	int lo_irq;
-	struct completion *sync;
 };
 
-int get_dvfs_size(int cluster, int cpu, int *size);
-int get_dvfs_capabilities(int cluster, int cpu, int *freqs);
-int get_performance(int cluster, int cpu, int *perf);
-int set_performance(int cluster, int cpu, int index);
+struct arm_mhu_request {
+	struct list_head list;
+	u32 cmd;
+	struct completion sync;
+	u8 *payload;
+	u32 payload_size;
+};
 
-static inline unsigned int mhu_reg_readl(struct arm_mhu_data *data, unsigned int reg, unsigned int value)
+int get_dvfs_size(int cluster, int cpu, u32 *size);
+int get_dvfs_capabilities(int cluster, int cpu, u32 *freqs, u32 size);
+int get_performance(int cluster, int cpu, u32 *perf);
+int set_performance(int cluster, int cpu, u32 index);
+
+static inline u8 mhu_reg_readb(struct arm_mhu_data *data, u32 reg)
+{
+	return readb(data->regs + reg);
+}
+
+static inline void mhu_reg_writeb(struct arm_mhu_data *data, u32 reg, u8 value)
+{
+	writeb(value, data->regs + reg);
+}
+
+static inline u32 mhu_reg_readl(struct arm_mhu_data *data, u32 reg)
 {
 	return readl(data->regs + reg);
 }
 
-static inline void mhu_reg_writel(struct arm_mhu_data *data, unsigned int reg, unsigned int value)
+static inline void mhu_reg_writel(struct arm_mhu_data *data, u32 reg, u32 value)
 {
 	writel(value, data->regs + reg);
 }
 
+static inline u8 mhu_mem_readb(struct arm_mhu_data *data, u32 addr)
+{
+	return readb(data->mem + addr);
+}
+
+static inline void mhu_mem_writeb(struct arm_mhu_data *data, u32 addr, u8 value)
+{
+	writeb(value, data->mem + addr);
+}
+
+static inline u32 mhu_mem_readl(struct arm_mhu_data *data, u32 addr)
+{
+	return readl(data->mem + addr);
+}
+
+static inline void mhu_mem_writel(struct arm_mhu_data *data, u32 addr, u32 value)
+{
+	writel(value, data->mem + addr);
+}
 #endif /* _ARM_MHU_H  */
