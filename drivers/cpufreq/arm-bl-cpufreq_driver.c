538d4eb1cf2acff6cd75c0a20d270d6d3525106a
Dave Martin <dave.martin@linaro.org>
2012-03-16 17:53:43 +0000

    cpufreq/arm-bl-cpufreq: Add simple cpufreq big.LITTLE switcher frontend
    This patch adds a very simple cpufreq-based frontend to the ARM
    big.LITTLE switcher.
    
    This driver simply simulates two performance points corresponding
    to the big and little clusters.
    
    Note that this driver requires the ARM switcher implementation to
    be loaded in order to work.  The switcher must have been built with
    ASYNC = FALSE in big-little/Makefile in order for the synchronous
    switching interface to be exposed.
    
    Bugs and limitations:
    
      * Switching twice in quick succession currently tends to cause a
        deadlock inside the switcher.  I still need to identify exactly
        what is going wrong here.
    
      * There is currently no tracing interface, and no interface for
        reporting what the dummy frequencies exposed by the driver
        actually mean in terms of real cluster / performance point
        combinations.  For the very simple case supported,
        cpuinfo_max_freq corresponds to big and cpuinfo_min_freq
        corresponds to LITTLE.
    
      * cpufreq will trigger spurious extra cluster switches.  This is
        a "feature" since I didn't tell cpufreq that this matters, but
        it may not be desirable.  Setting policy->cpus can probably fix
        this.
    
    Signed-off-by: Dave Martin <dave.martin@linaro.org>

---
+++ drivers/cpufreq/arm-bl-cpufreq_driver.c
@@ -0,0 +1,216 @@
+/*
+ * arm-bl-cpufreq.c: Simple cpufreq backend for the ARM big.LITTLE switcher
+ * Copyright (C) 2012  Linaro Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/* WARNING: This code is experimental and depends on external firmware */
+
+#define MODULE_NAME "arm-bl-cpufreq"
+#define pr_fmt(fmt) MODULE_NAME ": " fmt
+
+#include <linux/bug.h>
+#include <linux/cache.h>
+#include <linux/cpufreq.h>
+#include <linux/cpumask.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/string.h>
+#include <linux/spinlock.h>
+
+#include "arm-bl-cpufreq.h"
+
+/* Dummy frequencies representing the big and little clusters: */
+#define FREQ_BIG	1000000
+#define FREQ_LITTLE	 100000
+
+/*  Cluster numbers */
+#define CLUSTER_BIG	0
+#define CLUSTER_LITTLE	1
+
+static DEFINE_SPINLOCK(switcher_lock);
+
+static struct cpufreq_frequency_table __read_mostly bl_freqs[] = {
+	{ CLUSTER_BIG,		FREQ_BIG		},
+	{ CLUSTER_LITTLE,	FREQ_LITTLE		},
+	{ 0,			CPUFREQ_TABLE_END	},
+};
+
+
+/* Miscellaneous helpers */
+
+static unsigned int cluster_to_freq(int cluster)
+{
+	unsigned int i;
+
+	for(i = 0; bl_freqs[i].frequency != CPUFREQ_TABLE_END; i++)
+		if(bl_freqs[i].index == cluster)
+			return bl_freqs[i].frequency;
+
+	WARN(1, pr_fmt("%s(): invalid cluster number %d, assuming 0\n"),
+		__func__, cluster);
+	return bl_freqs[0].frequency;
+}
+
+/*
+ * Functions to get the current status.
+ *
+ * If you intend to use the result (i.e., it's not just for diagnostic
+ * purposes) then you should be holding switcher_lock ... otherwise
+ * the current cluster may change unexpectedly.
+ */
+static int get_current_cluster(void)
+{
+	return (__arm_bl_get_cluster() >> 8) & 0xF;
+}
+
+static unsigned int get_current_freq(void)
+{
+	return cluster_to_freq(get_current_cluster());
+}
+
+/*
+ * Switch to the requested cluster.
+ * There is no "switch_to_frequency" function, because the cpufreq frequency
+ * table helpers can easily look up the appropriate cluster number for us.
+ */
+static void switch_to_cluster(int cluster)
+{
+	pr_info("Switching to cluster %d\n", cluster);
+
+	spin_lock(&switcher_lock);
+	if(cluster != get_current_cluster())
+		__arm_bl_switch_cluster();
+	spin_unlock(&switcher_lock);
+}
+
+
+/* Cpufreq methods and module code */
+
+static int bl_cpufreq_init(struct cpufreq_policy *policy)
+{
+	int err;
+
+	/*
+	 * Set CPU and policy min and max frequencies based on bl_freqs:
+	 */
+	err = cpufreq_frequency_table_cpuinfo(policy, bl_freqs);
+	if (err)
+		goto error;
+
+	/*
+	 * No need for locking here:
+	 * cpufreq is not active until initialisation has finished.
+	 * Ideally, transition_latency should be calibrated here.
+	 */
+	policy->cpuinfo.transition_latency = CPUFREQ_ETERNAL;
+	policy->cur = get_current_freq();
+
+	/*
+	 * A b.L switch can be triggered from any CPU, but will affect them all.
+	 * The set of related CPUs should perhaps be determined from the
+	 * system CPU topology, rather than just the set of CPUs present...
+	 */
+	policy->shared_type = CPUFREQ_SHARED_TYPE_ANY;
+	cpumask_copy(policy->related_cpus, cpu_present_mask);
+	/*
+	 * We do not set ->cpus here, because it doesn't actually matter if
+	 * we try to switch on two CPUs at the same time.  Setting ->cpus
+	 * to cpu_present_mask might provide a way to avoid the need to take
+	 * switcher_lock when switching, though.
+	 */
+
+	pr_info("cpufreq initialised successfully\n");
+	return 0;
+
+error:
+	pr_warning("cpufreq initialisation failed (%d)\n", err);
+	return err;
+}
+
+static int bl_cpufreq_verify(struct cpufreq_policy *policy)
+{
+	return cpufreq_frequency_table_verify(policy, bl_freqs);
+}
+
+static int bl_cpufreq_target(struct cpufreq_policy *policy,
+			     unsigned int target_freq,
+			     unsigned int relation)
+{
+	int err;
+	int index;
+
+	err = cpufreq_frequency_table_target(policy, bl_freqs, target_freq,
+					     relation, &index);
+	if(err)
+		return err;
+
+	switch_to_cluster(bl_freqs[index].index);
+	return 0;
+}
+
+static unsigned int bl_cpufreq_get(unsigned int __always_unused cpu)
+{
+	/*
+	 * The cpu argument is ignored, because all CPUs have the same
+	 * performance point, by definition.
+	 */
+	return get_current_freq();
+}
+
+static struct cpufreq_driver __read_mostly bl_cpufreq_driver = {
+	.owner = THIS_MODULE,
+	.name = MODULE_NAME,
+
+	.init = bl_cpufreq_init,
+	.verify = bl_cpufreq_verify,
+	.target = bl_cpufreq_target,
+	.get = bl_cpufreq_get,
+	/* what else? */
+};
+
+static int __init bl_cpufreq_module_init(void)
+{
+	int err;
+
+	err = cpufreq_register_driver(&bl_cpufreq_driver);
+	if(err)
+		pr_info("cpufreq backend driver registration failed (%d)\n",
+			err);
+	else
+		pr_info("cpufreq backend driver registered.\n");
+
+	return err;
+}
+module_init(bl_cpufreq_module_init);
+
+static void __exit bl_cpufreq_module_exit(void)
+{
+	cpufreq_unregister_driver(&bl_cpufreq_driver);
+
+	/* Restore the "default" cluster: */
+	switch_to_cluster(CLUSTER_BIG);
+
+	pr_info("cpufreq backend driver unloaded.\n");
+}
+module_exit(bl_cpufreq_module_exit);
+
+
+MODULE_AUTHOR("Dave Martin");
+MODULE_DESCRIPTION("Simple cpufreq interface for the ARM big.LITTLE switcher");
+MODULE_LICENSE("GPL");
