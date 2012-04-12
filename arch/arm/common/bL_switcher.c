9189adc2b4df1c641991480a67fb9427b8657178
Nicolas Pitre <nicolas.pitre@linaro.org>
2012-04-12 02:56:10 -0400

    ARM: b.L: core switcher code
    Preliminary code.  Functional but could still be optimized further.
    
    At the moment, the inbound CPU perform the power down sequence and the
    outbound CPU wait for it to be done.  This avoids a race where the
    outbound CPU would have powered itself down after the inbound CPU
    changed its mind and attempted to switch back right away.
    
    Something more involved is still needed to eventually handle the CCI which
    can be done only after the caches have been disabled and cleaned.
    
    TODO:
    
    - Allow the outbound CPU to remain online for the inbound CPU to snoop its
      cache for a while.
    - Perform a switch during initialization to probe what the counterpart
      CPU's GIC interface ID is and stop hardcoding them in the code.
    - Local timers migration (they are not supported at the moment).
    - Handle the CCI after the cache is disabled.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>

---
+++ arch/arm/common/bL_switcher.c
@@ -0,0 +1,236 @@
+/*
+ * arch/arm/common/bL_switcher.c -- big.LITTLE cluster switcher core driver
+ *
+ * Created by:	Nicolas Pitre, March 2012
+ * Copyright:	(C) 2012  Linaro Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * TODO:
+ *
+ * - Allow the outbound CPU to remain online for the inbound CPU to snoop its
+ *   cache for a while.
+ * - Perform a switch during initialization to probe what the counterpart
+ *   CPU's GIC interface ID is and stop hardcoding them in the code.
+ * - Local timers migration (they are not supported at the moment).
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/interrupt.h>
+#include <linux/cpu_pm.h>
+
+#include <asm/suspend.h>
+#include <asm/cache.h>
+#include <asm/cacheflush.h>
+#include <asm/hardware/gic.h>
+#include <asm/bL_switcher.h>
+#include <asm/bL_entry.h>
+
+
+/*
+ * Use our own MPIDR accessors as the generic ones in asm/cputype.h have
+ * __attribute_const__ and we don't want the compiler to assume any
+ * constness here.
+ */
+
+static int read_mpidr(void)
+{
+	unsigned int id;
+	asm volatile ("mrc\tp15, 0, %0, c0, c0, 5" : "=r" (id));
+	return id;
+}
+
+/*
+ * bL switcher core code.
+ */
+
+const struct bL_power_ops *bL_platform_ops;
+
+extern void setup_mm_for_reboot(void);
+
+typedef void (*phys_reset_t)(unsigned long);
+
+static void bL_do_switch(void *_handshake_ptr)
+{
+	unsigned mpidr, cpuid, clusterid, ob_cluster, ib_cluster;
+	long volatile handshake = -1, **handshake_ptr = _handshake_ptr;
+	long handshake_result;
+	phys_reset_t phys_reset;
+	
+	pr_debug("%s\n", __func__);
+
+	/* Advertise our handshake location */
+	*handshake_ptr = &handshake;
+
+	mpidr = read_mpidr();
+	cpuid = mpidr & 0xf;
+	clusterid = (mpidr >> 8) & 0xf;
+	ob_cluster = clusterid;
+	ib_cluster = clusterid ^ 1;
+
+	/*
+	 * Our state has been saved at this point.  Let's release our
+	 * inbound CPU.
+	 */
+	bL_set_entry_vector(cpuid, ib_cluster, cpu_resume);
+	sev();
+
+	/*
+	 * From this point, we must assume that our counterpart CPU might 
+	 * have taken over in its parallel world already, as if execution
+	 * just returned from cpu_suspend().  It is therefore important to
+	 * be very careful not to make any change the other guy is not
+	 * expecting.  This is why we need stack isolation.
+	 */
+
+	/*
+	 * Let's wait for our counterpart to set our power state
+	 * and shake our hand good-bye.
+	 */
+	do {
+		wfe();
+		smp_mb();
+		handshake_result = handshake;
+	} while (handshake_result == -1);
+
+	/*
+	 * Now let's take care of cleaning our cache and
+	 * shutting ourself down.  If we're the last CPU in this cluster,
+	 * clean L2 too.
+	 */
+	setup_mm_for_reboot();
+	cpu_proc_fin();
+	flush_cache_all();
+	if (handshake_result != 0)
+		outer_flush_all();
+
+	/* And our own life ends right here... */
+	wfi();
+
+	/*
+	 * Hey, we're not dead!  This means a request to switch back
+	 * has come from our counterpart and reset was deasserted before
+	 * we had the chance to enter it.  Let's turn off the MMU and
+	 * branch back directly through our kernel entry point.
+	 */
+	phys_reset = (phys_reset_t)(unsigned long)virt_to_phys(cpu_reset);
+	phys_reset(virt_to_phys(bl_entry_point));
+
+	/* should never get here */
+	BUG();
+}
+
+/*
+ * Stack isolation (size needs to be optimized)
+ */
+
+static unsigned long __attribute__((__aligned__(L1_CACHE_BYTES)))
+	stacks[BL_CPUS_PER_CLUSTER][BL_NR_CLUSTERS][128];
+	
+extern void call_with_stack(void (*fn)(void *), void *arg, void *sp);
+
+static int bL_switchpoint(unsigned long handshake_p)
+{
+	unsigned int mpidr = read_mpidr();
+	unsigned int cpuid = mpidr & 0xf;
+	unsigned int clusterid = (mpidr >> 8) & 0xf;
+	void *stack = stacks[cpuid][clusterid] + ARRAY_SIZE(stacks[0][0]);
+	call_with_stack(bL_do_switch, (void*)handshake_p, stack);
+	BUG();
+}
+
+/*
+ * Generic switcher interface
+ */
+
+/*
+ * bL_switch_to - Switch to a specific cluster for the current CPU
+ * @new_cluster_id: the ID of the cluster to switch to.
+ *
+ * This function must be called on the CPU to be switched, with IRQs off.
+ * Returns 0 on success, else a negative status code.
+ */
+int bL_switch_to(unsigned int new_cluster_id)
+{
+	unsigned int mpidr, cpuid, clusterid, ob_cluster, ib_cluster;
+	long volatile *handshake_ptr;
+	int ret = 0;
+
+	local_fiq_disable();
+
+	mpidr = read_mpidr();
+	cpuid = mpidr & 0xf;
+	clusterid = (mpidr >> 8) & 0xf;
+	ob_cluster = clusterid;
+	ib_cluster = clusterid ^ 1;
+
+	if (new_cluster_id == clusterid)
+		goto out;
+
+	if (!bL_platform_ops) {
+		ret = -ENOSYS;
+		goto out;
+	}
+
+	pr_debug("before switch: CPU %d in cluster %d\n", cpuid, clusterid);
+
+	/* Close the gate for our entry vectors */
+	bL_set_entry_vector(cpuid, ob_cluster, NULL);
+	bL_set_entry_vector(cpuid, ib_cluster, NULL);
+
+	/*
+	 * Let's wake up the inbound CPU now in case it requires some delay
+	 * to come online, but leave it gated in our entry vector code.
+	 */
+	bL_platform_ops->power_up(cpuid, ib_cluster);
+
+	/* redirect GIC's SGIs to our counterpart */
+	gic_migrate_target(cpuid + ib_cluster*4);
+
+	/*
+	 * Raise a SGI on the inbound CPU to make sure it doesn't stall
+	 * in a possible WFI, such as the one in bL_do_switch().
+	 */
+	arm_send_ping_ipi(smp_processor_id());
+
+	ret = cpu_pm_enter();
+	if (ret)
+		goto out;
+
+	/* Let's do the actual CPU switch. */
+	ret = cpu_suspend((unsigned long)&handshake_ptr, bL_switchpoint);
+	if (ret > 0)
+		ret = -EINVAL;
+
+	/* We are executing on the inbound CPU at this point */
+	mpidr = read_mpidr();
+	cpuid = mpidr & 0xf;
+	clusterid = (mpidr >> 8) & 0xf;
+	pr_debug("after switch: CPU %d in cluster %d\n", cpuid, clusterid);
+	BUG_ON(clusterid != ib_cluster);
+
+	ret = cpu_pm_exit();
+
+	/* Now let's take care of shutting the outbound CPU down. */
+	*handshake_ptr = bL_platform_ops->power_down(cpuid, ob_cluster);
+	dsb_sev();
+       
+out:
+	if (ret)
+		pr_err("%s exiting with error %d\n", __func__, ret);
+	local_fiq_enable();
+	return ret;
+}
+
+EXPORT_SYMBOL_GPL(bL_switch_to);
+
+int __init bL_switcher_init(const struct bL_power_ops *ops)
+{
+	bL_platform_ops = ops;
+	pr_info("big.LITTLE switcher initialized\n");
+	return 0;
+}
