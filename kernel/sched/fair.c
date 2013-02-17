c42821f9b2d416abeee30670f132d8953bc45385
Alex Shi <alex.shi@intel.com>
2013-02-17 14:03:45 +0800

    sched: add new sg/sd_lb_stats fields for incoming fork/exec/wake balancing
    For power aware balancing, we care the sched domain/group's
    utilizations more than their load weight. So add:
    sd_lb_stats.sd_utils and sg_lb_stats.group_utils.
    
    We want to know the sd capacity, so add:
    sd_lb_stats.sd_capacity.
    
    And want to know which group is busiest but still has free time to
    handle more tasks, so add:
    sd_lb_stats.group_leader.
    
    Signed-off-by: Alex Shi <alex.shi@intel.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4205,6 +4205,11 @@ struct sd_lb_stats {
 	unsigned int  busiest_group_weight;
 
 	int group_imb; /* Is there imbalance in this sd */
+
+	/* Varibles of power awaring scheduling */
+	unsigned int  sd_utils;	/* sum utilizations of this domain */
+	unsigned long sd_capacity;	/* capacity of this domain */
+	struct sched_group *group_leader; /* Group which relieves group_min */
 };
 
 /*
@@ -4220,6 +4225,7 @@ struct sg_lb_stats {
 	unsigned long group_weight;
 	int group_imb; /* Is there an imbalance in the group ? */
 	int group_has_capacity; /* Is there extra capacity in the group? */
+	unsigned int group_utils;	/* sum utilizations of group */
 };
 
 /**
