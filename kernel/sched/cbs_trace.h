e49405b98a50aa4d8ecbd443e1a423292242d6ae
Patrick Bellasi <derkling@gmail.com>
2013-10-24 13:00:39 +0200

    [core/sched/cbs] Add FTrace events
    A couple of FTrace events have been added t report bursts and rounds
    data. These events belong to a new "sched_cbs" system which allows for
    an easy enabling/disabling of all events realted to CBS scheduler.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

---
+++ kernel/sched/cbs_trace.h
@@ -0,0 +1,104 @@
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM sched_cbs
+
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_FILE cbs_trace
+
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH ../../kernel/sched
+
+#if !defined(_TRACE_CBS_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_CBS_H
+
+#include <linux/sched.h>
+#include <linux/tracepoint.h>
+#include <linux/binfmts.h>
+
+/*
+ * Tracepoint for CBS burst completion
+ */
+TRACE_EVENT(cbs_burst,
+
+	TP_PROTO(struct sched_cbs_entity *cbs_se),
+
+	TP_ARGS(cbs_se),
+
+	TP_STRUCT__entry(
+		__field(	u32,	round_quota     )
+		__field(	u32,	burst_time_sp	)
+		__field(	u32,	burst_time	)
+		__field(	u64,	burst_interval	)
+		__field(	u64,	exec_runtime	)
+	),
+
+	TP_fast_assign(
+		__entry->round_quota	= cbs_se->round_quota;
+		__entry->burst_time_sp	= cbs_se->burst_time_sp;
+		__entry->burst_time	= cbs_se->burst_time;
+		__entry->burst_interval	= cbs_se->burst_interval_ns;
+		__entry->exec_runtime	= cbs_se->exec_runtime;
+	),
+
+
+	TP_printk("alfa=%u Tt_SP=%u b=%Lu Tt=%u exec=%Lu",
+		__entry->round_quota,
+		__entry->burst_time_sp,
+		__entry->burst_interval,
+		__entry->burst_time,
+		__entry->exec_runtime)
+);
+
+/*
+ * Tracepoint for CBS round completion
+ */
+TRACE_EVENT(cbs_round,
+
+	TP_PROTO(struct cbs_rq *cbs_rq),
+
+	TP_ARGS(cbs_rq),
+
+	TP_STRUCT__entry(
+		__field( unsigned int,	nr_running	)
+		__field( u64,		round_time_sp	)
+		__field( u64,		round_time	)
+		__field( u64,		round_time_next	)
+		__field( u64,		round_correction)
+		__field( u64,		round_error	)
+		__field( u64,		exec_runtime	)
+		__field( u8,		all_saturated	)
+		__field( u8,		clamp_rt	)
+		__field( u8,		needs_reinit	)
+	),
+
+	TP_fast_assign(
+		__entry->nr_running		= cbs_rq->nr_running;
+		__entry->round_time_sp		= cbs_rq->round_time_sp;
+		__entry->round_time		= cbs_rq->round_time;
+		__entry->round_time_next	= cbs_rq->round_time_next;
+		__entry->round_correction	= cbs_rq->round_correction;
+		__entry->round_error		= cbs_rq->round_error;
+		__entry->exec_runtime		= cbs_rq->exec_runtime;
+		__entry->all_saturated		= cbs_rq->all_saturated;
+		__entry->clamp_rt		= cbs_rq->clamp_rt;
+		__entry->needs_reinit		= cbs_rq->needs_reinit;
+	),
+
+
+	TP_printk("exec=%Lu | Rtime=%Lu [%c] Rerr=%Lu ===> Nr=%u Rt_SP=%Lu [%c%c] Rt_corr=%Lu Rt_next=%Lu ",
+		__entry->exec_runtime,
+		__entry->round_time,
+		__entry->clamp_rt ? 'c' : '-',
+		__entry->round_error,
+		__entry->nr_running,
+		__entry->round_time_sp,
+		__entry->needs_reinit ? 'r' : '-',
+		__entry->all_saturated ? 's' : '-',
+		__entry->round_correction,
+		__entry->round_time_next)
+);
+
+#endif /* _TRACE_CBS_H */
+
+/* This part must be outside protection */
+#include <trace/define_trace.h>
+
