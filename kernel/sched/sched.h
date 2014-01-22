b6e191c97f0c2640be56f7afffc5cafdd362d219
Dietmar Eggemann <dietmar.eggemann@arm.com>
2014-01-22 19:29:22 +0000

    sched: Aggregate unweighted task and blocked load on cfs_rq
    On a parenting cfs_rq we can aggregate the unweighted (uw) contribution of
    a task(t) as:
    
      uw_runnable_load(cfs_rq) = \Sum uw_task_load(t), for all runnable
                                                       children t
    
    Maintain it as cfs_rq.uw_runnable_load_avg as the uw counterpart of
    cfs_rq.runnable_load_avg.
    
    Maintain cfs_rq.uw_blocked_load_avg to account for uw blocked load as the
    uw counterpart of cfs_rq.blocked_load_avg.
    
    Maintain cfs_rq.uw_removed_load to account for uw migrated load as the uw
    counterpart of cfs_rq.removed_load.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -337,9 +337,10 @@ struct cfs_rq {
 	 * the FAIR_GROUP_SCHED case).
 	 */
 	unsigned long runnable_load_avg, blocked_load_avg;
+	unsigned long uw_runnable_load_avg, uw_blocked_load_avg;
 	atomic64_t decay_counter;
 	u64 last_decay;
-	atomic_long_t removed_load;
+	atomic_long_t removed_load, uw_removed_load;
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	/* Required to track per-cpu representation of a task_group */
