f3ae365e0c1ed83a680c19a8b0e36694b2835b18
Dietmar Eggemann <dietmar.eggemann@arm.com>
2014-01-22 17:35:20 +0000

    sched: Compute an unweighted version of task load contribution
    For a given task t, we can compute its unweighted (uw) contribution to
    load as:
    
      uw_task_load(t) = runnable_avg(t) * NICE_0_LOAD
    
    Maintain it as sched_avg.uw_load_avg_contrib as the uw counterpart of
    sched_avg.load_avg_contrib.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -96,6 +96,7 @@ static void print_cfs_group_stats(struct seq_file *m, int cpu, struct task_group
 	P(se->avg.runnable_avg_sum);
 	P(se->avg.runnable_avg_period);
 	P(se->avg.load_avg_contrib);
+	P(se->avg.uw_load_avg_contrib);
 	P(se->avg.decay_count);
 #endif
 #undef PN
@@ -635,6 +636,7 @@ void proc_sched_show_task(struct task_struct *p, struct seq_file *m)
 	P(se.avg.runnable_avg_sum);
 	P(se.avg.runnable_avg_period);
 	P(se.avg.load_avg_contrib);
+	P(se.avg.uw_load_avg_contrib);
 	P(se.avg.decay_count);
 #endif
 	P(policy);
