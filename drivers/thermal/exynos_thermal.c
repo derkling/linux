b2ccec552e92b4040e16bcf3f9db196ff72518a3
Punit Agrawal <punit.agrawal@arm.com>
2014-03-14 11:15:16 +0000

    thermal: exynos: Remove const qualifier
    Change-Id: I26b4181d7361ac919b896de8d949b7cf28f55403

--- drivers/thermal/exynos_thermal.c
+++ drivers/thermal/exynos_thermal.c
@@ -693,7 +693,7 @@ static int __ref exynos_throttle_cpu_hotplug(struct thermal_zone_device *thermal
 #endif
 
 /* Operation callback functions for thermal zone */
-static struct thermal_zone_device_ops const exynos_dev_ops = {
+static struct thermal_zone_device_ops exynos_dev_ops = {
 	.bind = exynos_bind,
 	.unbind = exynos_unbind,
 	.get_temp = exynos_get_temp,
