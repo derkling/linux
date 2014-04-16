b1299c2690f5bf3617614769bc9dd3a1fc19ab92
Steffen Klassert <steffen.klassert@secunet.com>
2014-04-16 09:01:03 +0200

    vti4: Don't count header length twice.
    We currently count the size of LL_MAX_HEADER and struct iphdr
    twice for vti4 devices, this leads to a wrong device mtu.
    The size of LL_MAX_HEADER and struct iphdr is already counted in
    ip_tunnel_bind_dev(), so don't do it again in vti_tunnel_init().
    
    Fixes: b9959fd3 ("vti: switch to new ip tunnel code")
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/ipv4/ip_vti.c
+++ net/ipv4/ip_vti.c
@@ -350,6 +350,7 @@ static int vti_tunnel_init(struct net_device *dev)
 	memcpy(dev->broadcast, &iph->daddr, 4);
 
 	dev->hard_header_len	= LL_MAX_HEADER + sizeof(struct iphdr);
+	dev->type		= ARPHRD_TUNNEL;
 	dev->mtu		= ETH_DATA_LEN;
 	dev->flags		= IFF_NOARP;
 	dev->iflink		= 0;
