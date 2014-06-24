0add3b1af16a002c56f970f79fc50544573b6f53
Daniel Lezcano <daniel.lezcano@linaro.org>
2014-06-24 19:16:21 +0200

    cpuidle: Add a simple irq governor
    This simple governor takes into account the predictable events: the timer sleep
    duration and the next expected irq sleep duration. By mixing both it deduced
    what idle state fits better.
    
    The main purpose of this governor is to handle the guessed next events in a
    categorized way:
    	1. deterministic events : timers
    	2. guessed events : IOs
    	3. predictable events : keystroke, incoming network packet, ...
    
    This governor is aimed to be moved later near the scheduler, so this one
    can inspect/inject more informations and act proactively rather than
    reactively.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    
    Conflicts:
    	drivers/cpuidle/Kconfig

---
+++ drivers/cpuidle/governors/irq.c
@@ -0,0 +1,33 @@
+/*
+ * irq.c - the irq governor
+ *
+ * Copyright (C) 2014 Daniel Lezcano <daniel.lezcano@linaro.org>
+ *
+*/
+#include <linux/ktime.h>
+#include <linux/irq.h>
+#include <linux/cpuidle.h>
+
+static int select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
+		  int latency_req, s64 next_timer_event)
+{
+	s64 next_irq_event = irqt_get_next_prediction(dev->cpu);
+	s64 next_event = next_irq_event ? 
+		min(next_irq_event, next_timer_event) : next_timer_event; 
+
+	return cpuidle_find_state(drv, dev, next_event, latency_req);
+}
+
+static struct cpuidle_governor irq_governor = {
+	.name   = "irq",
+	.rating = 10,
+	.select = select,
+	.owner  = THIS_MODULE,
+};
+
+static int __init irq_init(void)
+{
+	return cpuidle_register_governor(&irq_governor);
+}
+
+postcore_initcall(irq_init);
