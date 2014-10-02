d990beed660b4b5cc41da64a6968f49d3ef68cad
Patrick Bellasi <patrick.bellasi@arm.com>
2014-10-02 14:38:16 +0100

    lw-hotplug: Refactor _cpu_down to get a new cpu_clear flag
    EXPERIMENTAL - UNSTABLE - NOT FOR MAINLINE
    
    The cpu_clear flag will be used to enable the clear feature instead of
    a complete CPU shout-down.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -321,7 +321,7 @@ static int __ref take_cpu_down(void *_param)
 }
 
 /* Requires cpu_add_remove_lock to be held */
-static int __ref _cpu_down(unsigned int cpu, int tasks_frozen)
+static int __ref _cpu_down(unsigned int cpu, int tasks_frozen, bool cpu_clear)
 {
 	int err, nr_calls = 0;
 	void *hcpu = (void *)(long)cpu;
@@ -414,7 +414,7 @@ int __ref cpu_down(unsigned int cpu)
 		goto out;
 	}
 
-	err = _cpu_down(cpu, 0);
+	err = _cpu_down(cpu, 0, false);
 
 out:
 	cpu_maps_update_done();
@@ -529,7 +529,7 @@ int disable_nonboot_cpus(void)
 		if (cpu == first_cpu)
 			continue;
 		trace_suspend_resume(TPS("CPU_OFF"), cpu, true);
-		error = _cpu_down(cpu, 1);
+		error = _cpu_down(cpu, 1, false);
 		trace_suspend_resume(TPS("CPU_OFF"), cpu, false);
 		if (!error)
 			cpumask_set_cpu(cpu, frozen_cpus);
