844cff3d88effe6f57f4ea8082c9bcbca667c74d
Michael Turquette <mturquette@linaro.org>
2014-10-21 23:07:31 -0700

    sched: cpufreq_cfs: pelt-based cpu frequency scaling
    Scheduler-driven cpu frequency selection is desirable as part of the
    on-going effort to make the scheduler better aware of energy
    consumption.  No piece of the Linux kernel has a better view of the
    factors that affect a cpu frequency selection policy than the
    scheduler[0], and this patch is an attempt to converge on an initial
    solution.
    
    This patch implements a cpufreq governor that directly accesses
    scheduler statistics, in particular per-runqueue capacity utilization
    data from cfs via cfs.utilization_load_avg.
    
    Put plainly, this governor selects the lowest cpu frequency that will
    prevent a runqueue from being over-utilized (until we hit the highest
    frequency of course). This is accomplished by requesting a frequency
    that matches the current capacity utilization, plus a margin.
    
    Unlike the previous posting from 2014[1] this governor implements a
    "follow the utilization" method, where utilization is defined as the
    frequency-invariant product of cfs.utilization_load_avg and
    cpu_capacity_orig.
    
    This governor is event-driven. There is no polling loop to check cpu
    idle time nor any other method which is unsynchronized with the
    scheduler. The entry points for this policy are in fair.c:
    enqueue_task_fair, dequeue_task_fair and task_tick_fair.
    
    This policy is implemented using the cpufreq governor interface for two
    main reasons:
    
    1) re-using the cpufreq machine drivers without using the governor
    interface is hard.
    
    2) using the cpufreq interface allows us to switch between the
    scheduler-driven policy and legacy cpufreq governors such as ondemand at
    run-time. This is very useful for comparative testing and tuning.
    
    Finally, it is worth mentioning that this approach neglects all
    scheduling classes except for cfs. It is possible to add support for
    deadline and other other classes here, but I also wonder if a
    multi-governor approach would be a more maintainable solution, where the
    cpufreq core aggregates the constraints set by multiple governors.
    Supporting such an approach in the cpufreq core would also allow for
    peripheral devices to place constraint on cpu frequency without having
    to hack such behavior in at the governor level.
    
    Thanks to Juri Lelli <juri.lelli@arm.com> for contributing design ideas,
    code and test results.
    
    [0] http://article.gmane.org/gmane.linux.kernel/1499836
    [1] https://lkml.org/lkml/2014/10/22/22
    
    Signed-off-by: Michael Turquette <mturquette@linaro.org>
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    
    Conflicts:
    	kernel/sched/fair.c
    Trivial fix due to the presence of cpu_overutilized() check in
    task_tick_fair().

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4340,6 +4340,11 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		    cpu_overutilized(rq->cpu))
 			rq->rd->overutilized = true;
 	}
+
+	if(sched_energy_freq())
+		cpufreq_cfs_update_cpu(cpu_of(rq),
+				rq->cfs.utilization_load_avg);
+
 	hrtick_update(rq);
 }
 
@@ -4401,6 +4406,11 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		sub_nr_running(rq, 1);
 		update_rq_runnable_avg(rq, 1);
 	}
+
+	if(sched_energy_freq())
+		cpufreq_cfs_update_cpu(cpu_of(rq),
+				rq->cfs.utilization_load_avg);
+
 	hrtick_update(rq);
 }
 
@@ -8526,6 +8536,10 @@ static void task_tick_fair(struct rq *rq, struct task_struct *curr, int queued)
 
 	if (!rq->rd->overutilized && cpu_overutilized(task_cpu(curr)))
 		rq->rd->overutilized = true;
+
+	if(sched_energy_freq())
+		cpufreq_cfs_update_cpu(cpu_of(rq),
+				rq->cfs.utilization_load_avg);
 }
 
 /*
