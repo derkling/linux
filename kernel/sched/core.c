04193d2b0534f5f0dc398ccde98219add2c0a342
Patrick Bellasi <patrick.bellasi@arm.com>
2014-11-10 11:55:49 +0000

    lw-hotplug: notify CFS to properly update RQ loads
    EXPERIMENTAL - UNSTABLE - NOT FOR MAINLINE
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5146,6 +5146,7 @@ migration_call(struct notifier_block *nfb, unsigned long action, void *hcpu)
 	switch (action & ~CPU_TASKS_FROZEN) {
 
 	case CPU_UP_PREPARE:
+	case CPU_POPULATE:
 		rq->calc_load_update = calc_load_update;
 		break;
 
@@ -5175,6 +5176,7 @@ migration_call(struct notifier_block *nfb, unsigned long action, void *hcpu)
 		break;
 
 	case CPU_DEAD:
+	case CPU_CLEAN:
 		calc_load_migrate(rq);
 		break;
 #endif
