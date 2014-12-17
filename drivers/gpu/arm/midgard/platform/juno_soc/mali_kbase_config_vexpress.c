6d935e629cb250193cb10bc5be70d227ee1d75d2
Ørjan Eide <orjan.eide@arm.com>
2014-12-17 20:32:15 +0100

    mali: Update to use v6 IPA API
    Signed-off-by: Ørjan Eide <orjan.eide@arm.com>
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- drivers/gpu/arm/midgard/platform/juno_soc/mali_kbase_config_vexpress.c
+++ drivers/gpu/arm/midgard/platform/juno_soc/mali_kbase_config_vexpress.c
@@ -8,11 +8,10 @@
  */
 
 #include <linux/ioport.h>
+#include <linux/devfreq_cooling.h>
 #include <mali_kbase.h>
 #include <mali_kbase_defs.h>
 #include <mali_kbase_config.h>
-
-#include "../mali_kbase_power_actor.h"
 
 /* Versatile Express (VE) Juno Development Platform */
 
@@ -57,33 +56,35 @@ static struct kbase_pm_callback_conf pm_callbacks = {
 	.power_resume_callback = NULL
 };
 
-static unsigned long juno_model_static_power(unsigned long voltage, unsigned long temperature)
+static unsigned long juno_model_static_power(unsigned long voltage)
 {
 	/* Calculate power, corrected for voltage.
 	 * Shifts are done to avoid overflow. */
-	const unsigned long coefficient = (410UL << 20) / (729000000UL >> 10);
-	const unsigned long voltage_cubed = (voltage * voltage * voltage) >> 10;
+	const unsigned long coefficient =
+		(410UL << 20) / (729000000UL >> 10);
+	const unsigned long voltage_cubed =
+		(voltage * voltage * voltage) >> 10;
 
-	/* Calculate the temperature scaling factor. To be applied to the
-	 * voltage scaled power. */
-	const unsigned long temp = temperature / 1000;
-	const unsigned long temp_squared = temp * temp;
-	const unsigned long temp_cubed = temp_squared * temp;
-	const unsigned long temp_scaling_factor =
-			(2 * temp_cubed)
-			- (80 * temp_squared)
-			+ (4700 * temp)
-			+ 32000;
-
-	return (((coefficient * voltage_cubed) >> 20) * temp_scaling_factor) / 1000000;
+	return (((coefficient * voltage_cubed) >> 20));
 }
 
-static unsigned long juno_model_dynamic_power(unsigned long freq)
+static unsigned long juno_model_dynamic_power(unsigned long freq,
+		unsigned long voltage)
 {
-	return ((3600) * (freq / (1000 * 1000))) / 1000; /* mW */
+	/* The inputs: freq (f) is in Hz, and voltage (v) in mV.
+	 * The coefficient (c) is in mW/(MHz mV mV).
+	 *
+	 * This function calculates the dynamic power after this formula:
+	 * Pdyn (mW) = c (mW/(MHz*mV*mV)) * v (mV) * v (mV) * f (MHz)
+	 */
+	const unsigned long v2 = (voltage * voltage) / 1000; /* m*(V*V) */
+	const unsigned long f_mhz = freq / 1000000; /* MHz */
+	const unsigned long coefficient = 3600; /* mW/(MHz*mV*mV) */
+
+	return (coefficient * v2 * f_mhz) / 1000000; /* mW */
 }
 
-static struct mali_pa_model_ops juno_model_ops = {
+struct devfreq_cooling_ops juno_model_ops = {
 	.get_static_power = juno_model_static_power,
 	.get_dynamic_power = juno_model_dynamic_power,
 };
