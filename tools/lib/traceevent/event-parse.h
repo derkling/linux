c562658647cd51842428117d29da6df03588e61e
Javi Merino <javi.merino@arm.com>
2015-01-19 15:56:18 +0000

    tools lib traceevent: Add support for __print_array()
    Trace can now generate traces with variable element size arrays.  Add
    support to parse them.
    
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Steven Rostedt <srostedt@redhat.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Signed-off-by: Javi Merino <javi.merino@arm.com>

--- tools/lib/traceevent/event-parse.h
+++ tools/lib/traceevent/event-parse.h
@@ -230,6 +230,12 @@ struct print_arg_hex {
 	struct print_arg	*size;
 };
 
+struct print_arg_int_array {
+	struct print_arg	*field;
+	struct print_arg	*size;
+	struct print_arg	*el_size;
+};
+
 struct print_arg_dynarray {
 	struct format_field	*field;
 	struct print_arg	*index;
@@ -258,6 +264,7 @@ enum print_arg_type {
 	PRINT_FLAGS,
 	PRINT_SYMBOL,
 	PRINT_HEX,
+	PRINT_INT_ARRAY,
 	PRINT_TYPE,
 	PRINT_STRING,
 	PRINT_BSTRING,
@@ -276,6 +283,7 @@ struct print_arg {
 		struct print_arg_flags		flags;
 		struct print_arg_symbol		symbol;
 		struct print_arg_hex		hex;
+		struct print_arg_int_array	int_array;
 		struct print_arg_func		func;
 		struct print_arg_string		string;
 		struct print_arg_op		op;
