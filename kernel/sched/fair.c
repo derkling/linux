e1b7637907ea16cfd5346de23e72622b499f880d
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-01-28 15:56:22 +0000

    sched: Introduce energy awareness into detach_tasks
    Energy-aware load balancing does not rely on env->imbalance but instead it
    evaluates the system-wide energy difference for each task on the src rq by
    potentially moving it to the dst rq. If this energy difference is less
    than zero the task is actually moved from src to dst rq.
    
    Since the tasks are only removed from the src rq and not added to the dst
    rq in the while loop of detach_tasks, usage_detached holds the sum of
    usage from tasks already detached so by adding usage_detached and the
    utilization of the task which is currently under investigation we get the
    correct results from the energy_diff and __cpu_overutilized function calls.
    
    The dst cpu is limited from pulling more than its remaining capacity worth
    of usage to avoid over-utilization.
    
    !!! Mention that w/ this approach only a local energy minima can be
        reached.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6106,12 +6106,12 @@ static int detach_tasks(struct lb_env *env)
 {
 	struct list_head *tasks = &env->src_rq->cfs_tasks;
 	struct task_struct *p;
-	unsigned long load;
-	int detached = 0;
+	unsigned long load = 0;
+	int detached = 0, usage_detached = 0;
 
 	lockdep_assert_held(&env->src_rq->lock);
 
-	if (env->imbalance <= 0)
+	if (!env->use_ea && env->imbalance <= 0)
 		return 0;
 
 	while (!list_empty(tasks)) {
@@ -6132,6 +6132,28 @@ static int detach_tasks(struct lb_env *env)
 		if (!can_migrate_task(p, env))
 			goto next;
 
+		if (env->use_ea) {
+			int util = task_utilization(p);
+			int delta = util + usage_detached;
+			struct energy_env eenv = {
+				.src_cpu = env->src_cpu,
+				.dst_cpu = env->dst_cpu,
+				.usage_delta = delta,
+			};
+			int e_diff = energy_diff(&eenv);
+
+			if (e_diff >= 0)
+				goto next;
+
+			/* Do not over-utilize the destination cpu */
+			if (__cpu_overutilized(env->dst_cpu, env->sd, delta))
+				goto next;
+
+			usage_detached += util;
+
+			goto detach;
+		}
+
 		load = task_h_load(p);
 
 		if (sched_feat(LB_MIN) && load < 16 && !env->sd->nr_balance_failed)
@@ -6140,6 +6162,7 @@ static int detach_tasks(struct lb_env *env)
 		if ((load / 2) > env->imbalance)
 			goto next;
 
+detach:
 		detach_task(p, env);
 		list_add(&p->se.group_node, &env->tasks);
 
@@ -6160,7 +6183,7 @@ static int detach_tasks(struct lb_env *env)
 		 * We only want to steal up to the prescribed amount of
 		 * weighted load.
 		 */
-		if (env->imbalance <= 0)
+		if (!env->use_ea && env->imbalance <= 0)
 			break;
 
 		continue;
