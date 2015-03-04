c294849fe820c72c83b6ace73d6b05476679057e
Kapileshwar Singh <kapileshwar.singh@arm.com>
2015-03-04 12:32:34 +0000

    thermal: cpu_cooling: Fix power calculation when CPUs are offline
    We need to make sure that the CPU for which the frequency is being
    requested in a cpufreq_cooling_device is online. If none of the
    CPUs are online we return the requested power as 0.
    
    Signed-off-by: Kapileshwar Singh <kapileshwar.singh@arm.com>
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -691,7 +691,15 @@ static int cpufreq_get_requested_power(struct thermal_cooling_device *cdev,
 	struct cpufreq_cooling_device *cpufreq_device = cdev->devdata;
 	u32 *load_cpu = NULL;
 
-	freq = cpufreq_quick_get(cpumask_any(&cpufreq_device->allowed_cpus));
+	cpu = cpumask_any_and(&cpufreq_device->allowed_cpus, cpu_online_mask);
+
+	/* None of allowed_cpus are online */
+	if (cpu >= nr_cpu_ids) {
+		*power = 0;
+		return -ENODEV;
+	}
+
+	freq = cpufreq_quick_get(cpu);
 
 	if (trace_thermal_power_cpu_get_power_enabled()) {
 		u32 ncpus = cpumask_weight(&cpufreq_device->allowed_cpus);
