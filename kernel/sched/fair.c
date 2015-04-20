af0bcc2085badf3f743599e9acb45774c260928d
Morten Rasmussen <morten.rasmussen@arm.com>
2015-04-20 14:57:03 +0100

    sched: Energy model support for vacating entire sched_groups
    When searching for energy consumption optima it is useful to be able to
    test whether energy can be saved by vacating a whole sched_group despite
    that it may require multiple steps.
    
    struct energy_env has a new member, src_grp, added. When !NULL all cpus
    in that group have zero usage for energy model evaluation purposes
    (disappears). energy_env.usage and .dst_cpu may be set to add usage
    elsewhere.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4439,6 +4439,7 @@ struct energy_env {
 	int			usage_delta;
 	int			src_cpu;
 	int			dst_cpu;
+	struct sched_group	*src_grp;
 	int			energy;
 };
 
@@ -4478,6 +4479,9 @@ static inline int calc_usage_delta(struct energy_env *eenv, int cpu)
 		return -eenv->usage_delta;
 	if (cpu == eenv->dst_cpu)
 		return eenv->usage_delta;
+	if (eenv->src_grp && cpumask_test_cpu(cpu,
+				sched_group_cpus(eenv->src_grp)))
+		return -get_cpu_usage(cpu);
 	return 0;
 }
 
@@ -4658,7 +4662,17 @@ static int energy_diff(struct energy_env *eenv)
 	if (eenv->src_cpu == eenv->dst_cpu)
 		return 0;
 
-	sd_cpu = (eenv->src_cpu != -1) ? eenv->src_cpu : eenv->dst_cpu;
+	/*
+	 * Setting src_grp currently implies vacating the group. Set src_cpu
+	 * instead for the 'before' scenario.
+	 */
+	if (eenv->src_grp)
+		eenv_before.src_cpu =
+			cpumask_first(sched_group_cpus(eenv->src_grp));
+
+	sd_cpu = (eenv_before.src_cpu != -1) ? eenv_before.src_cpu
+						: eenv_before.dst_cpu;
+
 	sd = rcu_dereference(per_cpu(sd_ea, sd_cpu));
 
 	if (!sd)
@@ -4666,7 +4680,8 @@ static int energy_diff(struct energy_env *eenv)
 
 	sg = sd->groups;
 	do {
-		if (eenv->src_cpu != -1 && cpumask_test_cpu(eenv->src_cpu,
+		if (eenv_before.src_cpu != -1 &&
+				cpumask_test_cpu(eenv_before.src_cpu,
 							sched_group_cpus(sg))) {
 			eenv_before.sg_top = eenv->sg_top = sg;
 			energy_before += sched_group_energy(&eenv_before);
@@ -4676,7 +4691,8 @@ static int energy_diff(struct energy_env *eenv)
 			continue;
 		}
 
-		if (eenv->dst_cpu != -1	&& cpumask_test_cpu(eenv->dst_cpu,
+		if (eenv_before.dst_cpu != -1 &&
+				cpumask_test_cpu(eenv_before.dst_cpu,
 							sched_group_cpus(sg))) {
 			eenv_before.sg_top = eenv->sg_top = sg;
 			energy_before += sched_group_energy(&eenv_before);
