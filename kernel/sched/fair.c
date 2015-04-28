860efa8f481e6e12a227e8f66e719a88477ba205
Juri Lelli <juri.lelli@arm.com>
2015-04-28 14:10:57 +0100

    sched/cpufreq_sched_cfs: implement direct API
    Instead of using get_cpu_usage() we can let each CPU
    request the capacity it needs. The gov's kthread is
    responsible for aggregating requests.
    
    A benefit of this new API is shown in task_tick_fair(),
    where we can request a transition to max opp only when
    really needed.
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    
    Conflicts:
    	kernel/sched/fair.c
    	kernel/sched/sched.h
    
    Conflicts:
    	kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4106,7 +4106,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	}
 
 	if(sched_energy_freq())
-		gov_cfs_update_cpu(cpu_of(rq));
+		gov_cfs_update_cpu(cpu_of(rq), rq->cfs.utilization_load_avg);
 
 	hrtick_update(rq);
 }
@@ -4171,7 +4171,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	}
 
 	if(sched_energy_freq())
-		gov_cfs_update_cpu(cpu_of(rq));
+		gov_cfs_update_cpu(cpu_of(rq), rq->cfs.utilization_load_avg);
 
 	hrtick_update(rq);
 }
@@ -8101,6 +8101,7 @@ static void task_tick_fair(struct rq *rq, struct task_struct *curr, int queued)
 {
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &curr->se;
+	int cpu = task_cpu(curr);
 
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
@@ -8115,8 +8116,12 @@ static void task_tick_fair(struct rq *rq, struct task_struct *curr, int queued)
 	if (!rq->rd->overutilized && cpu_overutilized(task_cpu(curr)))
 		rq->rd->overutilized = true;
 
-	if(sched_energy_freq())
-		gov_cfs_update_cpu(cpu_of(rq));
+	if (sched_energy_freq() &&
+	    (capacity_curr_of(cpu) < capacity_orig_of(cpu)) &&
+	    ((capacity_curr_of(cpu) * 100) <
+	     (task_utilization(curr) * MARGIN_PCT))) {
+		gov_cfs_update_cpu(cpu_of(rq), SCHED_LOAD_SCALE);
+	}
 }
 
 /*
