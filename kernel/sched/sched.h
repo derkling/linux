cab8e9eaaf332080a0c2e261502327371d9fc5d0
Patrick Bellasi <patrick.bellasi@arm.com>
2015-05-16 23:47:16 +0100

    blmp: heterogeneous topologies support
    Introduce the basic SCHED_HMP infrastructure where each class of CPUs is
    represented by a "hmp_domain" and tasks will only be moved between these
    domains when their load profiles suggest it is beneficial.
    This patch adds the bits required to represent heterogeneous CPUs
    domains and properly configure a system to use them.
    
    .:: HMP Domains
    
    Platforms are required to implement the "arch_get_hmp_domains()"
    to set up the platform specific list of hmp_domains.
    There are no restrictions on the number of hmp_domains, however values
    greater than 2 has not been tested and the up/down migration policy is
    rather simple.
    
    It is also required that the platform disables SD_LOAD_BALANCE for the
    appropriate sched_domains. This is enforced at SD_CPU level by enabling
      CONFIG_DISABLE_CPU_SCHED_DOMAIN_BALANCE
    Moreover, there is currently no support for migration of task groups,
    hence the kernel should be configured with
      !CONFIG_SCHED_AUTOGROUP
    
    For the ARM big.LITTLE platform, hmp_domains could be defined either at
    compile time, via CPUs bitmask defined by:
      CONFIG_HMP_FAST_CPU_MASK
      CONFIG_HMP_SLOW_CPU_MASK
    or by FDT parsing, in case the previous masks are empty.
    
    .:: IRQs Affinity
    
    Once HMP domains are enabled it could make sense to tune IRQs in such a
    way that they hits just the most power efficient cores, while keeping
    quiet and/or not disturbing the more performing cores.
    Thus, by default IRQs affinity is set to match the lower domain.
    
    Signed-off-by: Morten Rasmussen <Morten.Rasmussen@arm.com>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Jon Medhurst <tixy@linaro.org

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -588,6 +588,9 @@ struct rq {
 	int active_balance;
 	int push_cpu;
 	struct cpu_stop_work active_balance_work;
+#ifdef CONFIG_SCHED_HMP
+	struct task_struct *migrate_task;
+#endif
 	/* cpu of this runqueue: */
 	int cpu;
 	int online;
@@ -810,6 +813,12 @@ extern int group_balance_cpu(struct sched_group *sg);
 #else
 
 static inline void sched_ttwu_pending(void) { }
+
+#ifdef CONFIG_SCHED_HMP
+static LIST_HEAD(hmp_domains);
+DECLARE_PER_CPU(struct hmp_domain *, hmp_cpu_domain);
+#define hmp_cpu_domain(cpu)	(per_cpu(hmp_cpu_domain, (cpu)))
+#endif /* CONFIG_SCHED_HMP */
 
 #endif /* CONFIG_SMP */
 
