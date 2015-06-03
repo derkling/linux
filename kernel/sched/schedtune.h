81b42ac8b0cb1ae1350d4c677c6647cee0cdaf51
Patrick Bellasi <patrick.bellasi@arm.com>
2015-06-03 15:49:19 +0100

    WIP: SchedTune: add boostmode selection attribute
    At least in the initial experimental integration phase of the SchedTune
    we want to evaluate easily different boosting modes.
    This patch adds the required CGroup attribute to select the booosting mode
    to be used by all tasks into a group.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/schedtune.h
+++ kernel/sched/schedtune.h
@@ -1,12 +1,30 @@
 #ifdef CONFIG_CGROUP_SCHEDTUNE
 
+enum schedtune_boostmode {
+	SCHEDTUNE_BOOSTMODE_NONE = 0,
+
+	SCHEDTUNE_BOOSTMODE_SPC,
+	SCHEDTUNE_BOOSTMODE_SPA,
+	SCHEDTUNE_BOOSTMODE_PSB,
+
+	/* Must be last entry */
+	SCHEDTUNE_BOOSTMODE_COUNT,
+};
+
 extern int schedtune_taskgroup_margin(struct task_struct *tsk);
+extern int schedtune_taskgroup_boostmode(struct task_struct *tsk);
 
 #else
 
 static int schedtune_taskgroup_margin(struct task_struct *tsk)
 {
 	return 0;
+}
+
+static int schedtune_taskgroup_boostmode(struct task_struct *tsk);
+
+{
+	return SCHEDTUNE_BOOSTMODE_NONE;
 }
 
 #endif
