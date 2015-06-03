87333ef1df006a4a4d1de908563aa7eb345d0946
Patrick Bellasi <patrick.bellasi@arm.com>
2015-06-03 16:18:12 +0100

    WIP: DEBUG: report on trace the computed CPU boost margin
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5442,6 +5442,9 @@ get_cpu_margin(unsigned long usage)
 
 	boost = schedtune_boost(usage, margin, boostmode);
 
+	trace_printk("usage=%lu boostmode=%d boost=%lu\n",
+			usage, boostmode, boost);
+
 	return boost;
 }
 
