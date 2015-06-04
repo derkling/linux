48af7ca546c59ef45da0c8983cee5da18e1598bf
Patrick Bellasi <patrick.bellasi@arm.com>
2015-06-04 11:30:52 +0100

    WIP: DEBUG: SchedTune: report energy_payoff regions
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/schedtune.c
+++ kernel/sched/schedtune.c
@@ -419,11 +419,13 @@ int schedtune_accept_deltas(int nrg_delta, int cap_delta, struct task_struct *ta
 
 	/* Optimal (O) region */
 	if (nrg_delta < 0 && cap_delta > 0) {
+		trace_printk("region=O ngain=0 pgain=0 nrg_payoff=-1");
 		return INT_MAX;
 	}
 
 	/* Suboptimal (S) region */
 	if (nrg_delta > 0 && cap_delta < 0) {
+		trace_printk("region=S ngain=0 pgain=0 nrg_payoff=1");
 		return -INT_MAX;
 	}
 
@@ -445,6 +447,11 @@ int schedtune_accept_deltas(int nrg_delta, int cap_delta, struct task_struct *ta
 		energy_payoff  = cap_delta * threshold_gains[perf_boost_idx].nrg_gain;
 		energy_payoff -= nrg_delta * threshold_gains[perf_boost_idx].cap_gain;
 
+		trace_printk("region=B ngain=%d pgain=%d nrg_payoff=%d",
+				threshold_gains[perf_boost_idx].nrg_gain,
+				threshold_gains[perf_boost_idx].cap_gain,
+				energy_payoff);
+
 		return energy_payoff;
 	}
 
@@ -458,6 +465,11 @@ int schedtune_accept_deltas(int nrg_delta, int cap_delta, struct task_struct *ta
 		 */
 		energy_payoff  = nrg_delta * threshold_gains[perf_constrain_idx].cap_gain;
 		energy_payoff -= cap_delta * threshold_gains[perf_constrain_idx].nrg_gain;
+
+		trace_printk("region=C ngain=%d pgain=%d nrg_payoff=%d",
+				threshold_gains[perf_constrain_idx].nrg_gain,
+				threshold_gains[perf_constrain_idx].cap_gain,
+				energy_payoff);
 
 		return energy_payoff;
 	}
