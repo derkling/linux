159a840333ae3ac045b11c4cf6246c37ecafcd75
Patrick Bellasi <patrick.bellasi@arm.com>
2015-06-04 11:31:59 +0100

    WIP: DEBUG: sched: report task boosted utilization
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5414,6 +5414,9 @@ get_taskgroup_margin(struct task_struct *task)
 
 	boost = schedtune_boost(utilization, margin, boostmode);
 
+	trace_printk("task=%d utilizaton=%lu boostmode=%d boost=%lu\n",
+			task->pid, utilization, boostmode, boost);
+
 	return boost;
 }
 
