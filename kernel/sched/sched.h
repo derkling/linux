913a9c4c726040f4fe882316cfe7369fcb442c2c
Juri Lelli <juri.lelli@arm.com>
2015-06-24 14:46:43 +0100

    sched/cpufreq_cfs: compute freq_new based on capacity_orig_of()
    capacity_new is both cpu and freq scaled with EAS. We thus need to
    compute freq_new using capacity_orig_of(), so that we properly scale
    back the thing on heterogeneous architectures.
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1481,6 +1481,8 @@ static inline unsigned long capacity_of(int cpu)
 	return cpu_rq(cpu)->cpu_capacity;
 }
 
+unsigned long capacity_orig_of(int cpu);
+
 extern struct static_key __sched_energy_freq;
 static inline bool sched_energy_freq(void)
 {
