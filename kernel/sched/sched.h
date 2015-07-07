885e51bad937c8098bb86a9a57e875eaf4529db9
Juri Lelli <juri.lelli@arm.com>
2015-07-07 19:24:23 +0100

    sched/cpufreq_sched: compute freq_new based on capacity_orig_of()
    capacity is both cpu and freq scaled with EAS. We thus need to compute
    freq_new using capacity_orig_of(), so that we properly scale back the thing
    on heterogeneous architectures. In fact, capacity_orig_of() returns only
    the cpu scaled part of capacity (see update_cpu_capacity()). So, to scale
    freq_new correctly, we multiply policy->max by capacity/capacity_orig_of()
    instead of capacity/1024.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1492,6 +1492,8 @@ unsigned long arch_scale_cpu_capacity(struct sched_domain *sd, int cpu)
 }
 #endif
 
+unsigned long capacity_orig_of(int cpu);
+
 extern struct static_key __sched_energy_freq;
 static inline bool sched_energy_freq(void)
 {
