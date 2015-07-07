970ee410708e90025e387273b9531beab372b66a
Patrick Bellasi <patrick.bellasi@arm.com>
2015-07-07 15:33:20 +0100

    WIP: sched/{fair,tune}: track RUNNABLE tasks impact on per CPU boost value
    When per-task boosting is enabled, every time a task enters/exits a CPU
    its boost value could impact the currently selected OPP for that CPU.
    Thus, the "aggregated" boost value for that CPU potentially needs to
    be updated to match the current maximum boost value among all the tasks
    currently RUNNABLE on that CPU.
    
    This patch introduces the required support to keep track of which boost
    groups are impacting a CPU. Each time a task is enqueued/dequeued to/from
    a CPU its boost group is used to increment a per-cpu counter of RUNNABLE
    tasks on that CPU.
    Only when the number of runnable tasks for a specific boost group
    becomes 1 or 0 the corresponding boost group changes its effects on
    that CPU, specifically:
      a) boost_group::tasks == 1: this boost group starts to impact the CPU
      b) boost_group::tasks == 0: this boost group stops to impact the CPU
    In each of these two conditions the aggregation function:
      sched_cpu_update(cpu)
    could be required to run in order to identify the new maximum boost
    value required for the CPU.
    
    The proposed patch minimizes the number of times the aggregation
    function is executed while still providing the required support to
    always boost a CPU to the maximum boost value required by all its
    currently RUNNABLE tasks.
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I345e36f349381a88f0caa14abb12f9f6b06b822a
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -34,6 +34,7 @@
 #include <trace/events/sched.h>
 
 #include "sched.h"
+#include "tune.h"
 
 /*
  * Targeted preemption latency for CPU-bound tasks:
@@ -4028,6 +4029,8 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		if (!task_new && !rq->rd->overutilized &&
 		    cpu_overutilized(rq->cpu))
 			rq->rd->overutilized = true;
+
+		schedtune_enqueue_task(p, cpu_of(rq));
 #endif
 	}
 	hrtick_update(rq);
@@ -4087,9 +4090,12 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		update_cfs_shares(cfs_rq);
 	}
 
-	if (!se)
+	if (!se) {
 		sub_nr_running(rq, 1);
-
+#ifdef CONFIG_SMP
+		schedtune_dequeue_task(p, cpu_of(rq));
+#endif
+	}
 	hrtick_update(rq);
 }
 
@@ -4962,10 +4968,15 @@ schedtune_margin(unsigned long signal, unsigned long boost)
 }
 
 static inline unsigned int
-schedtune_cpu_margin(unsigned long util)
+schedtune_cpu_margin(unsigned long util, int cpu)
 {
-	unsigned int boost = get_sysctl_sched_cfs_boost();
+	unsigned int boost;
 
+#ifdef CONFIG_CGROUP_SCHEDTUNE
+	boost = schedtune_cpu_boost(cpu);
+#else
+	boost = get_sysctl_sched_cfs_boost();
+#endif
 	if (boost == 0)
 		return 0;
 
@@ -4975,7 +4986,7 @@ schedtune_cpu_margin(unsigned long util)
 #else /* CONFIG_SCHED_TUNE */
 
 static inline unsigned int
-schedtune_cpu_margin(unsigned long util)
+schedtune_cpu_margin(unsigned long util, int cpu)
 {
 	return 0;
 }
@@ -4986,7 +4997,7 @@ static inline unsigned long
 boosted_cpu_util(int cpu)
 {
 	unsigned long util = cpu_util(cpu);
-	unsigned long margin = schedtune_cpu_margin(util);
+	unsigned long margin = schedtune_cpu_margin(util, cpu);
 
 	return util + margin;
 }
