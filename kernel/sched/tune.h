0380efa5fd4776ee705ae3855cee1739563cb8a7
Patrick Bellasi <patrick.bellasi@arm.com>
2015-07-07 15:33:20 +0100

    sched/{fair,tune}: track RUNNABLE tasks impact on per CPU boost value
    When per-task boosting is enabled, every time a task enter/exit a CPU
    its boost value could impact the currently selected OPP for that CPU.
    Thus, the "aggregated" boot value for that CPU potentially needs to
    be updated to match the current maximum boost value among all the tasks
    currently RUNNABLE on that CPU.
    
    This patch introduces the required support to keep track of which boost
    groups are impacting a CPU. Each time a task is enqueue/dequeued to/from
    a CPU its boost group is used to increment a per-cpu counter of RUNNABLE
    tasks on that CPU.
    Only when the number of runnable tasks for a specific boost group
    becomes 1 or 0 the corresponding boost group is changing its effects on
    that CPU, specifically:
      a) boost_group::tasks == 1: this boost group start to impact the CPU
      b) boost_group::tasks == 0: this boost group stop to impact the CPU
    In each of these two conditions the aggregation function:
      sched_cpu_update(cpu)
    could be required to run in order to idenfity the new maximum boost
    value required for the CPU.
    
    The proposed patch allows to reduce at minimum the number of times the
    aggregation function is executed while still providing the required
    support to always boost a CPU to the maximum boost value required by all
    its currently RUNNABLE tasks.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

---
+++ kernel/sched/tune.h
@@ -0,0 +1,21 @@
+
+#ifdef CONFIG_SCHED_TUNE
+
+#ifdef CONFIG_CGROUP_SCHEDTUNE
+
+extern void schedtune_enqueue_task(struct task_struct *p, int cpu);
+extern void schedtune_dequeue_task(struct task_struct *p, int cpu);
+
+#else /* CONFIG_CGROUP_SCHEDTUNE */
+
+#define schedtune_enqueue_task(task, cpu) while(0){}
+#define schedtune_dequeue_task(task, cpu) while(0){}
+
+#endif /* CONFIG_CGROUP_SCHEDTUNE */
+
+#else /* CONFIG_SCHED_TUNE */
+
+#define schedtune_enqueue_task(task, cpu) while(0){}
+#define schedtune_dequeue_task(task, cpu) while(0){}
+
+#endif /* CONFIG_SCHED_TUNE */
