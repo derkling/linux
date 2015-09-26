bb4c5d520cf73a2a850fe2ae1f3216f6abdd5da6
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-26 18:19:54 +0100

    ANDROID: sched: Update max cpu capacity in case of max frequency constraints
    Wakeup balancing uses cpu capacity awareness and needs to know the
    system-wide maximum cpu capacity.
    
    Patch "sched: Store system-wide maximum cpu capacity in root domain"
    finds the system-wide maximum cpu capacity during scheduler domain
    hierarchy setup. This is sufficient as long as maximum frequency
    invariance is not enabled.
    
    If it is enabled, the system-wide maximum cpu capacity can change
    between scheduler domain hierarchy setups due to frequency capping.
    
    The cpu capacity is changed in update_cpu_capacity() which is called in
    load balance on the lowest scheduler domain hierarchy level. To be able
    to know if a change in cpu capacity for a certain cpu also has an effect
    on the system-wide maximum cpu capacity it is normally necessary to
    iterate over all cpus. This would be way too costly. That's why this
    patch follows a different approach.
    
    The unsigned long max_cpu_capacity value in struct root_domain is
    replaced with a struct max_cpu_capacity, containing value (the
    max_cpu_capacity) and cpu (the cpu index of the cpu providing the
    maximum cpu_capacity).
    
    Changes to the system-wide maximum cpu capacity and the cpu index are
    made if:
    
     1 System-wide maximum cpu capacity < cpu capacity
     2 System-wide maximum cpu capacity > cpu capacity and cpu index == cpu
    
    There are no changes to the system-wide maximum cpu capacity in all
    other cases.
    
    Atomic read and write access to the pair (max_cpu_capacity.val,
    max_cpu_capacity.cpu) is enforced by max_cpu_capacity.lock.
    
    The access to max_cpu_capacity.val in task_fits_max() is still performed
    without taking the max_cpu_capacity.lock.
    
    The code to set max cpu capacity in build_sched_domains() has been
    removed because the whole functionality is now provided by
    update_cpu_capacity() instead.
    
    This approach can introduce errors temporarily, e.g. in case the cpu
    currently providing the max cpu capacity has its cpu capacity lowered
    due to frequency capping and calls update_cpu_capacity() before any cpu
    which might provide the max cpu now.
    
    Change-Id: Idaa7a16723001e222e476de34df332558e48dd13
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -304,6 +304,8 @@ static int init_rootdomain(struct root_domain *rd)
 
 	rd->max_cap_orig_cpu = rd->min_cap_orig_cpu = -1;
 
+	init_max_cpu_capacity(&rd->max_cpu_capacity);
+
 	return 0;
 
 free_cpudl:
@@ -1834,10 +1836,6 @@ build_sched_domains(const struct cpumask *cpu_map, struct sched_domain_attr *att
 		rq = cpu_rq(i);
 		sd = *per_cpu_ptr(d.sd, i);
 
-		/* Use READ_ONCE()/WRITE_ONCE() to avoid load/store tearing: */
-		if (rq->cpu_capacity_orig > READ_ONCE(d.rd->max_cpu_capacity))
-			WRITE_ONCE(d.rd->max_cpu_capacity, rq->cpu_capacity_orig);
-
 		if ((max_cpu < 0) || (cpu_rq(i)->cpu_capacity_orig >
 		    cpu_rq(max_cpu)->cpu_capacity_orig))
 			WRITE_ONCE(d.rd->max_cap_orig_cpu, i);
@@ -1849,11 +1847,6 @@ build_sched_domains(const struct cpumask *cpu_map, struct sched_domain_attr *att
 		cpu_attach_domain(sd, d.rd, i);
 	}
 	rcu_read_unlock();
-
-	if (rq && sched_debug_enabled) {
-		pr_info("span: %*pbl (max cpu_capacity = %lu)\n",
-			cpumask_pr_args(cpu_map), rq->rd->max_cpu_capacity);
-	}
 
 	ret = 0;
 error:
