8f868c019f127cf72f824d934a35c2638936e4d0
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2015-11-04 21:06:02 +0100

    powerpc: mmu nohash: Convert to hotplug state machine
    Install the callbacks via the state machine.
    
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: linuxppc-dev@lists.ozlabs.org (open list:LINUX FOR POWERPC (32-BIT AND 64-BIT))
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/cpuhotplug.h
+++ include/linux/cpuhotplug.h
@@ -37,6 +37,7 @@ enum cpuhp_state {
 	CPUHP_XEN_HVM_GUEST_PREPARE,	/* P: 0 S: xen_hvm_cpu_up_prepare T: NULL C: P */
 	CPUHP_S390_HWS_PREPARE,		/* P: 0 S: s390_hws_cpu_prepare T: s390_hws_cpu_prepare C: P */
 	CPUHP_POWER_PMAC_PREPARE,	/* P: 0 S: smp_core99_cpu_prepare T: NULL C: P */
+	CPUHP_POWER_MMU_CTX_PREPARE,	/* P: 0 S: mmu_ctx_cpu_prepare T: mmu_ctx_cpu_dead C: P */
 	CPUHP_NOTIFY_PREPARE,		/* P: CPU_UP_PREPARE S: notify_prepare: T: NULL C: C */
 	CPUHP_NOTIFY_DEAD,		/* P: CPU_DEAD S: NULL: T: notify_dead C: C */
 	CPUHP_X86_APB_DEAD,		/* P: -20 S: NULL T: apbt_cpu_dead C: I */
