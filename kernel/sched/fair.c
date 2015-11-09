ca6d9ac26892df6f9e0777ffa7424a7444d3260f
Juri Lelli <juri.lelli@arm.com>
2015-11-09 12:07:27 +0000

    DEBUG: sched: add tracepoint for task load/util signals
    Change-Id: Ia40312601bc15570de3dd84bd72dc9c6000ee19c
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2560,6 +2560,8 @@ static inline void update_load_avg(struct sched_entity *se, int update_tg)
 		se->avg.util_est = se->avg.util_avg;
 	}
 
+	if (entity_is_task(se))
+		trace_sched_load_avg_task(task_of(se), &se->avg);
 }
 
 static void attach_entity_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *se)
