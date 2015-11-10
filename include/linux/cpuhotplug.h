5c1db05c28f360b319bc2c9179e819076f19ccab
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2015-11-10 18:30:26 +0100

    watchdog: octeon: Convert to hotplug state machine
    Install the callbacks via the state machine and let the core invoke
    the callbacks on the already online CPUs.
    
    Cc: Wim Van Sebroeck <wim@iguana.be>
    Cc: linux-watchdog@vger.kernel.org
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/cpuhotplug.h
+++ include/linux/cpuhotplug.h
@@ -202,6 +202,7 @@ enum cpuhp_state {
 	CPUHP_SCSI_BNX2I_ONLINE,	/* P: 0 S: bnx2i_cpu_online T: NULL C: I */
 	CPUHP_SCSI_FCOE_ONLINE,		/* P: 0 S: fcoe_cpu_online T: NULL C: I */
 	CPUHP_X86_PKG_THERM_ONLINE,	/* P: 0 S: pkg_thermal_cpu_online T: pkg_thermal_cpu_pre_down C: I */
+	CPUHP_WDT_OCTEON_ONLINE,	/* P: 0 S: octeon_wdt_cpu_online T: octeon_wdt_cpu_pre_down C: I */
 	CPUHP_X86_HPET_ONLINE,		/* P: -20 S: hpet_cpuhp_online T: NULL C: I */
 	CPUHP_X86_KVM_CLK_ONLINE,	/* P: INT_MIN+1 S: kvmclock_cpu_online T: NULL C: I */
 	CPUHP_NOTIFY_ONLINE,		/* P: CPU_ONLINE S: notify_online T: NULL, C: C */
