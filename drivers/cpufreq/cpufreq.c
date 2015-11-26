4968287a7a5cb994660d22b3a9f0915975400f03
Juri Lelli <juri.lelli@arm.com>
2015-11-26 16:29:11 +0000

    cpufreq: hold policy->rwsem across CPUFREQ_GOV_POLICY_EXIT
    There are no good reasons why policy->rwsem cannot be hold across calls to
    __cpufreq_governor with CPUFREQ_GOV_POLICY_EXIT event.
    
    Remove {up,down}_write across such call sites. This also verify assertion
    that policy->rwsem is always hold when calling into __cpufreq_governor.
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2178,9 +2178,7 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 			return ret;
 		}
 
-		up_write(&policy->rwsem);
 		ret = __cpufreq_governor(policy, CPUFREQ_GOV_POLICY_EXIT);
-		down_write(&policy->rwsem);
 
 		if (ret) {
 			pr_err("%s: Failed to Exit Governor: %s (%d)\n",
@@ -2197,9 +2195,7 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 		if (!ret)
 			goto out;
 
-		up_write(&policy->rwsem);
 		__cpufreq_governor(policy, CPUFREQ_GOV_POLICY_EXIT);
-		down_write(&policy->rwsem);
 	}
 
 	/* new governor failed, so re-start old one */
