7d3d4bd837c47bba14bb8d6f1fc86969f7311173
Patrick Bellasi <patrick.bellasi@arm.com>
2015-12-16 17:16:31 +0000

    WIP: sched/fair: add support to use estimated util to drive CPUFreq
    Using a sched_feature, this patch allows to use the estimated CPU utilization
    signal to drive SchedFreq frequencies selections.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4224,6 +4224,12 @@ static inline void hrtick_update(struct rq *rq)
 static bool cpu_overutilized(int cpu);
 static unsigned long capacity_orig_of(int cpu);
 
+unsigned long cpu_util_est(int cpu);
+static inline bool use_util_est(void)
+{
+	return sched_feat(UTIL_EST);
+}
+
 static void update_capacity_of(int cpu)
 {
 	unsigned long req_cap;
@@ -4232,7 +4238,11 @@ static void update_capacity_of(int cpu)
 		return;
 
 	/* Convert scale-invariant capacity to cpu. */
-	req_cap = cpu_util(cpu) * SCHED_CAPACITY_SCALE / capacity_orig_of(cpu);
+	if (use_util_est())
+		req_cap = cpu_util_est(cpu);
+	else
+		req_cap = cpu_util(cpu);
+	req_cap = req_cap * SCHED_CAPACITY_SCALE / capacity_orig_of(cpu);
 	set_cfs_cpu_capacity(cpu, true, req_cap);
 }
 
