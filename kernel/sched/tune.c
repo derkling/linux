327fd448c3588e67fae54951fae070bc052047ea
Patrick Bellasi <patrick.bellasi@arm.com>
2016-06-08 18:41:51 +0100

    sched/tune: fix accounting for runnable tasks (5/5)
    The accounting for tasks into boost groups of different CPUs is currently
    broken mainly because:
    a) we do not properly track the change of boost group of a RUNNABLE task
    b) there are race conditions between migration code and accounting code
    
    To avoid accounting issues at startup, this patch disable the SchedTune
    accounting until the required data structures have been properly
    initialized.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -11,6 +11,10 @@
 #include "sched.h"
 #include "tune.h"
 
+#ifdef CONFIG_CGROUP_SCHEDTUNE
+static bool schedtune_initialized = false;
+#endif
+
 unsigned int sysctl_sched_cfs_boost __read_mostly;
 
 extern struct target_nrg schedtune_target_nrg;
@@ -360,6 +364,9 @@ void schedtune_enqueue_task(struct task_struct *p, int cpu)
 	struct schedtune *st;
 	int idx;
 
+	if (!unlikely(schedtune_initialized))
+		return;
+
 	/*
 	 * When a task is marked PF_EXITING by do_exit() it's going to be
 	 * dequeued and enqueued multiple times in the exit path.
@@ -404,6 +411,9 @@ int schedtune_can_attach(struct cgroup_subsys_state *css,
 	int src_bg; /* Source boost group index */
 	int dst_bg; /* Destination boost group index */
 	int tasks;
+
+	if (!unlikely(schedtune_initialized))
+		return 0;
 
 	cgroup_taskset_for_each(task, tset) {
 
@@ -482,6 +492,9 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu)
 	struct schedtune *st;
 	int idx;
 
+	if (!unlikely(schedtune_initialized))
+		return;
+
 	/*
 	 * When a task is marked PF_EXITING by do_exit() it's going to be
 	 * dequeued and enqueued multiple times in the exit path.
@@ -516,6 +529,9 @@ void schedtune_exit_task(struct task_struct *tsk)
 	unsigned int cpu;
 	struct rq *rq;
 	int idx;
+
+	if (!unlikely(schedtune_initialized))
+		return;
 
 	rq = lock_rq_of(tsk, &irq_flags);
 	rcu_read_lock();
@@ -709,6 +725,8 @@ schedtune_init_cgroups(void)
 
 	pr_info("schedtune: configured to support %d boost groups\n",
 		BOOSTGROUPS_COUNT);
+
+	schedtune_initialized = true;
 }
 
 #else /* CONFIG_CGROUP_SCHEDTUNE */
