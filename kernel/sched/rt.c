b2fa7af7dfa340a61b18ebc5a0e003e0c1409839
Juri Lelli <juri.lelli@arm.com>
2016-07-05 12:30:59 +0100

    sched/rt: disable dump of throttled tasks

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1032,7 +1032,7 @@ static int sched_rt_runtime_exceeded(struct rt_rq *rt_rq)
 
 			if (!once) {
 				once = true;
-				dump_throttled_rt_tasks(rt_rq);
+				//dump_throttled_rt_tasks(rt_rq);
 			}
 		} else {
 			/*
