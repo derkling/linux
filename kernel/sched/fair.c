79c62b973bcfc881a69013f541a16409322bfec5
Patrick Bellasi <patrick.bellasi@arm.com>
2016-07-14 15:41:27 +0100

    WIP: DEBUG: new energy diff function profiling support
    Let's run fist the new version to avoid any potential cache benefits on
    its execution time.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5039,7 +5039,8 @@ static inline bool cpu_in_sg(struct sched_group *sg, int cpu)
  * utilization is removed from or added to the system (e.g. task wake-up). If
  * both are specified, the utilization is migrated.
  */
-static inline int
+/* static inline int */
+noinline int
 __energy_diff(struct energy_env *eenv)
 {
 	struct sched_domain *sd;
@@ -5295,7 +5296,8 @@ __update_perf_energy_deltas(struct energy_env *eenv)
  *     energy diff for:      2
  *
  */
-static inline int
+/* static inline int */
+noinline int
 __energy_diff_new(struct energy_env *eenv)
 {
 	struct sched_domain *ea_sd, *sd;
@@ -5434,8 +5436,8 @@ energy_diff(struct energy_env *eenv)
 	int nrg_delta;
 
 	/* Conpute "absolute" energy diff */
-	__energy_diff(eenv);
 	__energy_diff_new(eenv);
+	__energy_diff(eenv);
 
 	/* Return energy diff when boost margin is 0 */
 	if (boost == 0)
