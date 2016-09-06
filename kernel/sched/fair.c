30e8b7f535bc3744a9e2be7206a3fd00a9c9f5f6
Patrick Bellasi <patrick.bellasi@arm.com>
2016-09-06 14:50:20 +0100

    NFM: sched/fair: use energy_env as single argument
    The energy_env data structure is used to cache values required by multiple
    different functions involved in energy_diff computation. Some of these
    functions require additional parameters which can be easily embedded
    into the energy_env itself.
    
    This patch update all the clients of energy_env to use only data
    provided by this structure. Moreover, to simplify the signature of
    tracepoints reporting energy_env data, its definition is moved into:
       include/linux/sched.h
    which is already included by:
       include/trace/events/sched.h
    where tracepoints are defined.
    
    cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4691,16 +4691,6 @@ static inline bool energy_aware(void)
 	return sched_feat(ENERGY_AWARE);
 }
 
-struct energy_env {
-	struct sched_group	*sg_top;
-	struct sched_group	*sg_cap;
-	int			cap_idx;
-	int			util_delta;
-	int			src_cpu;
-	int			dst_cpu;
-	int			energy;
-};
-
 /*
  * __cpu_norm_util() returns the cpu util relative to a specific capacity,
  * i.e. it's busy ratio, in the range [0..SCHED_LOAD_SCALE] which is useful for
@@ -4758,10 +4748,11 @@ unsigned long group_max_util(struct energy_env *eenv)
  * estimate (more busy).
  */
 static unsigned
-long group_norm_util(struct energy_env *eenv, struct sched_group *sg)
+long group_norm_util(struct energy_env *eenv)
 {
 	int i, delta;
 	unsigned long util_sum = 0;
+	struct sched_group *sg = eenv->sg;
 	unsigned long capacity = sg->sge->cap_states[eenv->cap_idx].cap;
 
 	for_each_cpu(i, sched_group_cpus(sg)) {
@@ -4774,20 +4765,20 @@ long group_norm_util(struct energy_env *eenv, struct sched_group *sg)
 	return util_sum;
 }
 
-static int find_new_capacity(struct energy_env *eenv,
-	const struct sched_group_energy const *sge)
+static int find_new_capacity(struct energy_env *eenv)
 {
-	int idx;
+	const struct sched_group_energy const *sge = eenv->sg->sge;
 	unsigned long util = group_max_util(eenv);
+	int idx;
 
 	for (idx = 0; idx < sge->nr_cap_states; idx++) {
 		if (sge->cap_states[idx].cap >= util)
 			break;
 	}
 
+	/* Keep track of SG's capacity index */
 	eenv->cap_idx = idx;
-
-	return idx;
+	return eenv->cap_idx;
 }
 
 static int group_idle_state(struct sched_group *sg)
@@ -4864,9 +4855,12 @@ static int sched_group_energy(struct energy_env *eenv)
 				else
 					eenv->sg_cap = sg;
 
-				cap_idx = find_new_capacity(eenv, sg->sge);
+				eenv->sg = sg;
+				cap_idx = find_new_capacity(eenv);
 				idle_idx = group_idle_state(sg);
-				group_util = group_norm_util(eenv, sg);
+
+				group_util = group_norm_util(eenv);
+
 				sg_busy_energy = (group_util * sg->sge->cap_states[cap_idx].power)
 								>> SCHED_CAPACITY_SHIFT;
 				sg_idle_energy = ((SCHED_LOAD_SCALE-group_util)
