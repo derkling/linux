b6ec84c0997935a0c14a5372a25d30bc238928d5
Ionela Voinescu <ionela.voinescu@arm.com>
2017-01-19 13:49:22 +0000

    drm: ARM HDLCD: sync hdlcd driver with GEM5 alpha
    Sync the HDLCD driver with the GEM5 alpha branch as to now worry
    about lacking functionality. Once these are synced we'll drop
    this commit.
    
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>

--- drivers/gpu/drm/arm/hdlcd_drv.h
+++ drivers/gpu/drm/arm/hdlcd_drv.h
@@ -8,24 +8,23 @@
 struct hdlcd_drm_private {
 	void __iomem			*mmio;
 	struct clk			*clk;
-	struct hdlcd_drm_fbdev		*fbdev;
+	struct drm_fbdev_cma		*fbdev;
 	struct drm_framebuffer		*fb;
-	struct drm_pending_vblank_event	*event;
+	struct list_head		event_list;
 	struct drm_crtc			crtc;
-	struct completion		frame_completion;
+	struct drm_plane		*plane;
 #ifdef CONFIG_DEBUG_FS
 	atomic_t buffer_underrun_count;
 	atomic_t bus_error_count;
 	atomic_t vsync_count;
 	atomic_t dma_end_count;
 #endif
-	int dpms;
 };
 
 #define crtc_to_hdlcd_priv(x)	container_of(x, struct hdlcd_drm_private, crtc)
 
-static inline void
-hdlcd_write(struct hdlcd_drm_private *hdlcd, unsigned int reg, u32 value)
+static inline void hdlcd_write(struct hdlcd_drm_private *hdlcd,
+			       unsigned int reg, u32 value)
 {
 	writel(value, hdlcd->mmio + reg);
 }
@@ -35,35 +34,9 @@ static inline u32 hdlcd_read(struct hdlcd_drm_private *hdlcd, unsigned int reg)
 	return readl(hdlcd->mmio + reg);
 }
 
-/*
- * Developers using HDLCD may wish to enable these settings if
- * display disruption is apparent and you suspect HDLCD
- * access to RAM may be starved.
- *
- * Turn HDLCD default color to red instead of default black so
- * that it's easier to see data underruns (compared to other
- * visual disruptions)
- */
-//#define HDLCD_SHOW_UNDERRUN
-
-/* setup the crtc subclass */
 int hdlcd_setup_crtc(struct drm_device *dev);
-
-/* functions for creating a suitable connector */
-extern int hdlcd_create_digital_connector(struct drm_device *dev,
-					struct hdlcd_drm_private *hdlcd);
-extern int hdlcd_create_vexpress_connector(struct drm_device *dev,
-					struct hdlcd_drm_private *hdlcd);
-#ifdef CONFIG_DRM_VIRTUAL_HDLCD
-extern int hdlcd_create_virtual_connector(struct drm_device *dev);
-#else
-static inline int hdlcd_create_virtual_connector(struct drm_device *dev)
-{
-	return -ENXIO;
-}
-#endif
-
-/* common function used by all connectors */
-extern struct drm_encoder *hdlcd_connector_best_encoder(struct drm_connector *con);
+void hdlcd_set_scanout(struct hdlcd_drm_private *hdlcd);
+void hdlcd_crtc_suspend(struct drm_crtc *crtc);
+void hdlcd_crtc_resume(struct drm_crtc *crtc);
 
 #endif /* __HDLCD_DRV_H__ */
