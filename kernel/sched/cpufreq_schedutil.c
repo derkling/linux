60cf3e16ae7ab2ecb23aa0a8e10b43659d3402af
Patrick Bellasi <patrick.bellasi@arm.com>
2017-01-31 13:42:58 +0000

    sched/{core,cpufreq_schedutil}: add capacity clamping for RT/DL tasks
    Currently schedutil enforce a maximum OPP when RT tasks are RUNNABLE.
    Such a mandatory policy can be made more tunable from userspace thus
    allowing for example to define a reasonable max frequency which is
    required for the execution of a specific RT workload. This will
    contribute to make the RT class more friendly for power/energy sensible
    applications.
    
    This patch extends the usage of capacity_{min,max} to the RT/DL classes.
    Whenever a task in these classes in RUNNABLE, the capacity required is
    defined by the constraints of the control group the task belong to.
    
    Change-Id: I6c992ae06e59842cfee90915a20c8cce32440d4b
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-pm@vger.kernel.org

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -203,7 +203,9 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 		return;
 
 	if (flags & SCHED_CPUFREQ_RT_DL) {
-		next_f = policy->cpuinfo.max_freq;
+		util = cap_group_clamp(SCHED_CAPACITY_SCALE,
+				       smp_processor_id());
+		next_f = get_next_freq(sg_cpu, util, policy->cpuinfo.max_freq);
 	} else {
 		sugov_get_util(&util, &max);
 		sugov_iowait_boost(sg_cpu, &util, &max);
@@ -219,12 +221,8 @@ static unsigned int sugov_next_freq_shared(struct sugov_cpu *sg_cpu,
 {
 	struct sugov_policy *sg_policy = sg_cpu->sg_policy;
 	struct cpufreq_policy *policy = sg_policy->policy;
-	unsigned int max_f = policy->cpuinfo.max_freq;
 	u64 last_freq_update_time = sg_policy->last_freq_update_time;
 	unsigned int j;
-
-	if (flags & SCHED_CPUFREQ_RT_DL)
-		return max_f;
 
 	sugov_iowait_boost(sg_cpu, &util, &max);
 	util = cap_group_clamp(util, smp_processor_id());
@@ -250,10 +248,11 @@ static unsigned int sugov_next_freq_shared(struct sugov_cpu *sg_cpu,
 			j_sg_cpu->iowait_boost = 0;
 			continue;
 		}
-		if (j_sg_cpu->flags & SCHED_CPUFREQ_RT_DL)
-			return max_f;
 
-		j_util = cap_group_clamp(j_sg_cpu->util, j);
+		if (j_sg_cpu->flags & SCHED_CPUFREQ_RT_DL)
+			j_util = cap_group_clamp(SCHED_CAPACITY_SCALE, j);
+		else
+			j_util = cap_group_clamp(j_sg_cpu->util, j);
 		j_max = j_sg_cpu->max;
 		if (j_util * max > j_max * util) {
 			util = j_util;
