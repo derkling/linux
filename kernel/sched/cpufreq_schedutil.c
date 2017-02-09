65ccb49c8f1e085244be33d548ef51b3537e8ac7
Patrick Bellasi <patrick.bellasi@arm.com>
2017-02-09 15:02:05 +0000

    cpufreq: schedutil: add util clamp for RT tasks
    Currently schedutil enforces a maximum frequency when RT tasks are
    RUNNABLE.  Such a mandatory policy can be made more tunable from
    userspace thus allowing for example to define a max frequency which is
    still reasonable for the execution of a specific RT workload. This
    will contribute to make the RT class more friendly for power/energy
    sensitive use-cases.
    
    This patch extends the usage of util_{min,max} to the RT scheduling
    class.  Whenever a task in this class is RUNNABLE, the util required is
    defined by the constraints of the CPU control group the task belongs to.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-pm@vger.kernel.org

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -279,7 +279,10 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	busy = sugov_cpu_is_busy(sg_cpu);
 
 	if (flags & SCHED_CPUFREQ_RT) {
-		next_f = policy->cpuinfo.max_freq;
+		util = uclamp_util(smp_processor_id(), SCHED_CAPACITY_SCALE);
+		next_f = (uclamp_enabled && util < SCHED_CAPACITY_SCALE)
+			? get_next_freq(sg_policy, util, policy->cpuinfo.max_freq)
+			: policy->cpuinfo.max_freq;
 	} else {
 		sugov_get_util(sg_cpu);
 		max = sg_cpu->max;
@@ -339,11 +342,15 @@ static unsigned int sugov_next_freq_shared(struct sugov_cpu *sg_cpu, u64 time)
 			if (j_sg_cpu->util_dl == 0)
 				continue;
 		}
-		if (j_sg_cpu->flags & SCHED_CPUFREQ_RT)
-			return policy->cpuinfo.max_freq;
+		if (j_sg_cpu->flags & SCHED_CPUFREQ_RT) {
+			if (!uclamp_enabled)
+				return policy->cpuinfo.max_freq;
+			j_util = uclamp_util(j, SCHED_CAPACITY_SCALE);
+		} else {
+			j_util = sugov_aggregate_util(j_sg_cpu);
+		}
 
 		j_max = j_sg_cpu->max;
-		j_util = sugov_aggregate_util(j_sg_cpu);
 		if (j_util * max > j_max * util) {
 			util = j_util;
 			max = j_max;
