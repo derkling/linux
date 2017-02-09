6622af2e94204a451b93567d6ed4d59606353590
Patrick Bellasi <patrick.bellasi@arm.com>
2017-02-09 22:57:57 +0000

    FROMLIST: sched/cpufreq: uclamp: add utilization clamping for FAIR tasks
    Each time a frequency update is required via schedutil, a frequency is
    selected to (possibly) satisfy the utilization reported by the CFS
    class. However, when utilization clamping is in use, the frequency
    selection should consider the requirements suggested by userspace, for
    example, to:
    
     - boost tasks which are directly affecting the user experience
       by running them at least at a minimum "required" frequency
    
     - cap low priority tasks not directly affecting the user experience
       by running them only up to a maximum "allowed" frequency
    
    These constraints are meant to support a per-task based tuning of the
    frequency selection thus allowing to have a fine grained definition of
    performance boosting vs energy saving strategies in kernel space.
    
    Let's add the required support to clamp the utilization generated by
    FAIR tasks within the boundaries defined by their aggregated utilization
    clamp constraints.
    On each CPU the aggregated clamp values are obtained by considering the
    maximum of the {min,max}_util values for each task. This max aggregation
    responds to the goal of not penalizing, for example, high boosted (i.e.
    more important for the user-experience) CFS tasks which happens to be
    co-scheduled with high capped (i.e. less important for the
    user-experience) CFS tasks.
    
    For FAIR tasks both the utilization as well as the IOWait boost values
    are clamped according to the CPU aggregated utilization clamp
    constraints.
    
    The default values for boosting and capping are defined to be:
     - util_min: 0
     - util_max: SCHED_CAPACITY_SCALE
    which means that by default no boosting/capping is enforced on FAIR
    tasks, and thus the frequency will be selected considering the actual
    utilization value of each CPU.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Quentin Perret <quentin.perret@arm.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-pm@vger.kernel.org
    [
     Interation provided for:
       type == frequency_util
     When type != frequency_util, i.e. we run the EM, there is currently NOT
     utilization clamping integraton. This has to be better defined!
    ]
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    
    ---
    Changes in v4:
     Message-ID: <CAKfTPtC2adLupg7wy1JU9zxKx1466Sza6fSCcr92wcawm1OYkg@mail.gmail.com>
     - use *rq instead of cpu for both uclamp_util() and uclamp_value()
     Message-ID: <20180816135300.GC2960@e110439-lin>
     - remove uclamp_value() which is never used outside CONFIG_UCLAMP_TASK
     Others:
     - rebased on v4.19-rc1
    
    Changes in v3:
     Message-ID: <CAJuCfpF6=L=0LrmNnJrTNPazT4dWKqNv+thhN0dwpKCgUzs9sg@mail.gmail.com>
     - rename UCLAMP_NONE into UCLAMP_NOT_VALID
     Others:
     - rebased on tip/sched/core
    Changes in v2:
     - rebased on v4.18-rc4

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -254,10 +254,15 @@ unsigned long schedutil_freq_util(int cpu, unsigned long util_cfs,
 	 * utilization (PELT windows are synchronized) we can directly add them
 	 * to obtain the CPU's actual utilization.
 	 */
-	util = util_cfs;
-	util += cpu_util_rt(rq);
-
 	if (type == FREQUENCY_UTIL) {
+
+		/*
+		 * CFS utilization can be boosted or capped, depending on utilization
+		 * clamp constraints configured for currently RUNNABLE tasks.
+		 */
+		util  = util_cfs ? uclamp_util(cpu, util_cfs) : 0;
+		util += cpu_util_rt(rq);
+
 		/*
 		 * For frequency selection we do not make cpu_util_dl() a
 		 * permanent part of this sum because we want to use
@@ -273,6 +278,9 @@ unsigned long schedutil_freq_util(int cpu, unsigned long util_cfs,
 		if ((util + cpu_util_dl(rq)) >= max)
 			return max;
 	} else {
+
+		util = util_cfs + cpu_util_rt(rq);
+
 		/*
 		 * OTOH, for energy computation we need the estimated
 		 * running time, so include util_dl and ignore dl_bw.
@@ -368,6 +376,7 @@ static void sugov_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 			       unsigned int flags)
 {
 	bool set_iowait_boost = flags & SCHED_CPUFREQ_IOWAIT;
+	unsigned int max_boost;
 
 	/* Reset boost if the CPU appears to have been idle enough */
 	if (sg_cpu->iowait_boost &&
@@ -383,11 +392,24 @@ static void sugov_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 		return;
 	sg_cpu->iowait_boost_pending = true;
 
+	/*
+	 * Boost FAIR tasks only up to the CPU clamped utilization.
+	 *
+	 * Since DL tasks have a much more advanced bandwidth control, it's
+	 * safe to assume that IO boost does not apply to those tasks.
+	 * Instead, since RT tasks are not utiliation clamped, we don't want
+	 * to apply clamping on IO boost while there is blocked RT
+	 * utilization.
+	 */
+	max_boost = sg_cpu->iowait_boost_max;
+	if (!cpu_util_rt(cpu_rq(sg_cpu->cpu)))
+		max_boost = uclamp_util(cpu_rq(sg_cpu->cpu), max_boost);
+
 	/* Double the boost at each request */
 	if (sg_cpu->iowait_boost) {
 		sg_cpu->iowait_boost <<= 1;
-		if (sg_cpu->iowait_boost > sg_cpu->iowait_boost_max)
-			sg_cpu->iowait_boost = sg_cpu->iowait_boost_max;
+		if (sg_cpu->iowait_boost > max_boost)
+			sg_cpu->iowait_boost = max_boost;
 		return;
 	}
 
