392ae11cb004126e33f6fd883694d2dd55dfad89
Patrick Bellasi <patrick.bellasi@arm.com>
2017-02-09 15:02:05 +0000

    sched/cpufreq: uclamp: add utilization clamping for RT tasks
    Currently schedutil enforces a maximum frequency when RT tasks are
    RUNNABLE.  Such a mandatory policy can be made more tunable from
    userspace thus allowing for example to define a max frequency which is
    still reasonable for the execution of a specific RT workload.
    This will contribute to make the RT class more friendly for power/energy
    sensitive use-cases.
    
    This patch extends the usage of util_{min,max} to the RT scheduling
    class. Whenever a task in this class is RUNNABLE, the util required is
    defined by its task specific clamp value. However, we still want to run
    at maximum capacity RT tasks which do not have task specific clamp
    values.
    
    Let's add uclamp_default_perf, a special set of clamp value to be used
    for tasks that require maximum performance. This set of clamps are then
    used whenever the above conditions matches for an RT task being enqueued
    on a CPU.
    
    Since utilization clamping applies now to both CFS and RT tasks, we
    clamp the combined utilization of these two classes.
    This approach, contrary to combining individually clamped utilizations,
    is more power efficient. Indeed, it selects lower frequencies when we
    have both RT and CFS clamped tasks.
    However, it could also affect performance of the lower priority CFS
    class, since the CFS's minimum utilization clamp could be completely
    eclipsed by the RT workloads.
    
    The IOWait boost value also is subject to clamping for RT tasks.
    This is to ensure that RT tasks as well as CFS ones are always subject
    to the set of current utilization clamping constraints.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Quentin Perret <quentin.perret@arm.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-pm@vger.kernel.org
    
    ---
    Changes in v5:
     Others:
     - rebased on v4.19
    
    Changes in v4:
     Message-ID: <20180813150112.GE2605@e110439-lin>
     - remove UCLAMP_SCHED_CLASS policy since we do not have in the current
       implementation a proper per-sched_class clamp tracking support
     Message-ID: <20180809155551.bp46sixk4u3ilcnh@queper01-lin>
     - add default boost for not clamped RT tasks
     Others:
     - rebased on v4.19-rc1
    Changes in v3:
     - rebased on tip/sched/core
    Changes in v2:
     - rebased on v4.18-rc4

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -204,9 +204,6 @@ unsigned long schedutil_freq_util(int cpu, unsigned long util_cfs,
 	struct rq *rq = cpu_rq(cpu);
 	unsigned long util, irq;
 
-	if (type == FREQUENCY_UTIL && rt_rq_is_runnable(&rq->rt))
-		return max;
-
 	/*
 	 * Early check to see if IRQ/steal time saturates the CPU, can be
 	 * because of inaccuracies in how we track these -- see
@@ -222,7 +219,7 @@ unsigned long schedutil_freq_util(int cpu, unsigned long util_cfs,
 	 * utilization (PELT windows are synchronized) we can directly add them
 	 * to obtain the CPU's actual utilization.
 	 */
-	util = cpu_util_rt(rq);
+	util = cpu_util_rt(rq) + util_cfs;
 
 	if (type == FREQUENCY_UTIL) {
 		/*
@@ -236,13 +233,13 @@ unsigned long schedutil_freq_util(int cpu, unsigned long util_cfs,
 		 * to not quite hit saturation when we should --
 		 * something for later.
 		 *
-		 * CFS utilization can be boosted or capped, depending on
-		 * utilization clamp constraints requested by currently
+		 * CFS and RT utilization can be boosted or capped, depending
+		 * on utilization clamp constraints requested by currently
 		 * RUNNABLE tasks.  When there are no CFS RUNNABLE tasks,
 		 * clamps are released and OPPs will be gracefully reduced
 		 * with the utilization decay.
 		 */
-		util += uclamp_util(rq, util_cfs);
+		util = uclamp_util(rq, util);
 		if ((util + cpu_util_dl(rq)) >= max)
 			return max;
 	} else {
@@ -250,7 +247,6 @@ unsigned long schedutil_freq_util(int cpu, unsigned long util_cfs,
 		 * OTOH, for energy computation we need the estimated
 		 * running time, so include util_dl and ignore dl_bw.
 		 */
-		util += util_cfs;
 		util += cpu_util_dl(rq);
 		if (util >= max)
 			return max;
@@ -364,13 +360,11 @@ static void sugov_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 	 *
 	 * Since DL tasks have a much more advanced bandwidth control, it's
 	 * safe to assume that IO boost does not apply to those tasks.
-	 * Instead, since RT tasks are not utiliation clamped, we don't want
-	 * to apply clamping on IO boost while there is blocked RT
-	 * utilization.
+	 * Instead, for CFS and RT tasks we clamp the IO boost max value
+	 * considering the current constraints for the CPU.
 	 */
 	max_boost = sg_cpu->iowait_boost_max;
-	if (!cpu_util_rt(cpu_rq(sg_cpu->cpu)))
-		max_boost = uclamp_util(cpu_rq(sg_cpu->cpu), max_boost);
+	max_boost = uclamp_util(cpu_rq(sg_cpu->cpu), max_boost);
 
 	/* Double the boost at each request */
 	if (sg_cpu->iowait_boost) {
