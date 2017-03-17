fcbdf4866866325bdfba9b758e77ebe2e986e2a1
Dietmar Eggemann <dietmar.eggemann@arm.com>
2017-03-17 20:27:06 +0000

    sched/events: Introduce cfs_rq load tracking trace event
    The following trace event keys are mapped to:
    
     (1) load     : cfs_rq->avg.load_avg
    
     (2) rbl_load : cfs_rq->avg.runnable_load_avg
    
     (2) util     : cfs_rq->avg.util_avg
    
    To let this trace event work for configurations w/ and w/o group
    scheduling support for cfs (CONFIG_FAIR_GROUP_SCHED) the following
    special handling is necessary for a non-existent key=value pair:
    
     path = "(null)" : In case of !CONFIG_FAIR_GROUP_SCHED.
    
    The following list shows examples of the key=value pairs in different
    configurations for:
    
     (1) a root task_group:
    
         cpu=4 path=/ load=6 rbl_load=6 util=331
    
     (2) a task_group:
    
         cpu=1 path=/tg1/tg11/tg111 load=538 rbl_load=538 util=522
    
     (3) an autogroup:
    
         cpu=3 path=/autogroup-18 load=997 rbl_load=997 util=517
    
     (4) w/o CONFIG_FAIR_GROUP_SCHED:
    
         cpu=0 path=(null) load=314 rbl_load=314 util=289
    
    The trace event is only defined for CONFIG_SMP.
    
    The helper function __trace_sched_path() can be used to get the length
    parameter of the dynamic array (path == NULL) and to copy the path into
    it (path != NULL).
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3317,6 +3317,9 @@ __update_load_avg_cfs_rq(u64 now, int cpu, struct cfs_rq *cfs_rq)
 				cfs_rq->curr != NULL)) {
 
 		___update_load_avg(&cfs_rq->avg, 1, 1);
+
+		trace_sched_load_cfs_rq(cfs_rq);
+
 		return 1;
 	}
 
@@ -3594,6 +3597,8 @@ static inline int propagate_entity_load_avg(struct sched_entity *se)
 	update_tg_cfs_util(cfs_rq, se, gcfs_rq);
 	update_tg_cfs_runnable(cfs_rq, se, gcfs_rq);
 
+	trace_sched_load_cfs_rq(cfs_rq);
+
 	return 1;
 }
 
@@ -3745,6 +3750,8 @@ static void attach_entity_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 	add_tg_cfs_propagate(cfs_rq, se->avg.load_sum);
 
 	cfs_rq_util_change(cfs_rq);
+
+	trace_sched_load_cfs_rq(cfs_rq);
 }
 
 /**
@@ -3764,6 +3771,8 @@ static void detach_entity_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 	add_tg_cfs_propagate(cfs_rq, -se->avg.load_sum);
 
 	cfs_rq_util_change(cfs_rq);
+
+	trace_sched_load_cfs_rq(cfs_rq);
 }
 
 /*
