7655294dfdf382b001ee17d57e7b269e8fcf25b4
Patrick Bellasi <patrick.bellasi@arm.com>
2017-03-27 13:58:26 +0100

    DEBUG: sched: trace estimated utilization
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- include/trace/events/sched.h
+++ include/trace/events/sched.h
@@ -578,9 +578,12 @@ TRACE_EVENT(sched_load_avg_task,
 		__field( int,	cpu				)
 		__field( unsigned long,	load_avg		)
 		__field( unsigned long,	util_avg		)
+		__field( unsigned long,	ewma			)
+		__field( unsigned long,	last			)
 		__field( u64,		load_sum		)
 		__field( u32,		util_sum		)
 		__field( u32,		period_contrib		)
+
 	),
 
 	TP_fast_assign(
@@ -589,20 +592,24 @@ TRACE_EVENT(sched_load_avg_task,
 		__entry->cpu			= task_cpu(tsk);
 		__entry->load_avg		= avg->load_avg;
 		__entry->util_avg		= avg->util_avg;
+		__entry->ewma			= avg->util_est.ewma;
+		__entry->last			= avg->util_est.last;
 		__entry->load_sum		= avg->load_sum;
 		__entry->util_sum		= avg->util_sum;
 		__entry->period_contrib		= avg->period_contrib;
 	),
 
-	TP_printk("comm=%s pid=%d cpu=%d load_avg=%lu util_avg=%lu load_sum=%llu"
-		  " util_sum=%u period_contrib=%u",
+	TP_printk("comm=%s pid=%d cpu=%d util_avg=%lu ewma=%lu last=%lu util_sum=%u "
+		  "load_avg=%lu load_sum=%llu period_contrib=%u",
 		  __entry->comm,
 		  __entry->pid,
 		  __entry->cpu,
-		  __entry->load_avg,
 		  __entry->util_avg,
-		  (u64)__entry->load_sum,
+		  __entry->ewma,
+		  __entry->last,
 		  (u32)__entry->util_sum,
+		  __entry->load_avg,
+		  (u64)__entry->load_sum,
 		  (u32)__entry->period_contrib)
 );
 
@@ -619,16 +626,21 @@ TRACE_EVENT(sched_load_avg_cpu,
 		__field( int,	cpu				)
 		__field( unsigned long,	load_avg		)
 		__field( unsigned long,	util_avg		)
+		__field( unsigned long,	last			)
 	),
 
 	TP_fast_assign(
 		__entry->cpu			= cpu;
 		__entry->load_avg		= cfs_rq->avg.load_avg;
 		__entry->util_avg		= cfs_rq->avg.util_avg;
+		__entry->last			= cfs_rq->avg.util_est.last;
 	),
 
-	TP_printk("cpu=%d load_avg=%lu util_avg=%lu",
-		  __entry->cpu, __entry->load_avg, __entry->util_avg)
+	TP_printk("cpu=%d util_avg=%lu last=%lu load_avg=%lu",
+		  __entry->cpu,
+		  __entry->util_avg,
+		  __entry->last,
+		  __entry->load_avg)
 );
 #endif /* _TRACE_SCHED_H */
 
