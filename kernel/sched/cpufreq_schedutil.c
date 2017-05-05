059ab99b7c5ec70500387c73ba15276e3fa06871
Vikram Mulukutla <markivx@codeaurora.org>
2017-05-04 19:47:06 -0700

    sched: cpufreq: Allow one cpu to update the util of another CPU
    Load tracking updates may happen for CPU X on another CPU Y. To allow
    for faster updates, have CPU X update CPU Y's utilization, so that
    the governor may respond faster.
    
    This is necessary for WALT migration updates, otherwise a migration
    invoked load update at the beginning of a window may only be known
    to the governor in the next window. Considering a window size of 20ms
    this is not a good idea; the task deserves the performance level
    it has already earned on its previous CPU/cluster.
    
    Change-Id: I0dc208b6548c5f96a34e59ffed747b1854699b3e
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -78,6 +78,7 @@ struct sugov_cpu {
 #ifdef CONFIG_NO_HZ_COMMON
 	unsigned long saved_idle_calls;
 #endif
+	unsigned int cpu;
 };
 
 static DEFINE_PER_CPU(struct sugov_cpu, sugov_cpu);
@@ -198,9 +199,9 @@ static inline bool use_pelt(void)
 #endif
 }
 
-static void sugov_get_util(unsigned long *util, unsigned long *max, u64 time)
+static void
+sugov_get_util(unsigned long *util, unsigned long *max, u64 time, int cpu)
 {
-	int cpu = smp_processor_id();
 	struct rq *rq = cpu_rq(cpu);
 	unsigned long max_cap, rt;
 	s64 delta;
@@ -290,7 +291,7 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	if (flags & SCHED_CPUFREQ_DL) {
 		next_f = policy->cpuinfo.max_freq;
 	} else {
-		sugov_get_util(&util, &max, time);
+		sugov_get_util(&util, &max, time, sg_cpu->cpu);
 		sugov_iowait_boost(sg_cpu, &util, &max);
 		next_f = get_next_freq(sg_policy, util, max);
 		/*
@@ -355,7 +356,7 @@ static void sugov_update_shared(struct update_util_data *hook, u64 time,
 	unsigned long util, max;
 	unsigned int next_f;
 
-	sugov_get_util(&util, &max, time);
+	sugov_get_util(&util, &max, time, sg_cpu->cpu);
 
 	raw_spin_lock(&sg_policy->update_lock);
 
@@ -746,6 +747,7 @@ static int sugov_start(struct cpufreq_policy *policy)
 
 		memset(sg_cpu, 0, sizeof(*sg_cpu));
 		sg_cpu->sg_policy = sg_policy;
+		sg_cpu->cpu = cpu;
 		sg_cpu->flags = SCHED_CPUFREQ_DL;
 		sg_cpu->iowait_boost_max = policy->cpuinfo.max_freq;
 		cpufreq_add_update_util_hook(cpu, &sg_cpu->update_util,
