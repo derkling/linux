a4cf31ada0a1e4bb9293427fcf2df205b240beb1
Patrick Bellasi <patrick.bellasi@arm.com>
2017-10-27 16:13:32 +0100

    DEBUG: add further tracepoints

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4898,6 +4898,10 @@ static inline void util_est_enqueue(struct task_struct *p)
 	 *       utilization estimation policy at run-time.
 	 */
 	cfs_rq->avg.util_est.last += task_util_est(p);
+
+	/* Update plots for Task and CPU estimated utilization */
+	trace_sched_load_avg_task(p, &p->se.avg);
+	trace_sched_load_avg_cpu(cpu_of(rq_of(cfs_rq)), cfs_rq);
 }
 
 /*
@@ -4995,6 +4999,10 @@ static inline void util_est_dequeue(struct task_struct *p, int flags)
 	p->se.avg.util_est.last = task_util(p);
 	ewma_util_add(&p->se.avg.util_ewma, p->se.avg.util_est.last);
 	p->se.avg.util_est.ewma = ewma_util_read(&p->se.avg.util_ewma);
+
+	/* Update plots for Task and CPU estimated utilization */
+	trace_sched_load_avg_task(p, &p->se.avg);
+	trace_sched_load_avg_cpu(cpu_of(rq_of(cfs_rq)), cfs_rq);
 }
 
 static void set_next_buddy(struct sched_entity *se);
