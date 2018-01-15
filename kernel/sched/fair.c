7a76b7ee1bb6b8148a1f4a7a6cecae40ad0beeff
Valentin Schneider <valentin.schneider@arm.com>
2018-01-15 12:35:23 +0000

    DEBUG: sched: fair: trace swb_tick

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8512,6 +8512,10 @@ static int should_we_balance(struct lb_env *env)
 	struct sched_group *sg = env->sd->groups;
 	int cpu, balance_cpu = -1;
 
+	trace_printk("swb_tick: cpu=%i sd=%lx env=%lx\n",
+		     env->dst_cpu, *cpumask_bits(sched_domain_span(env->sd)),
+		     *cpumask_bits(env->cpus));
+
 	/*
 	 * Ensure the balancing environment is consistent; can happen
 	 * when the softirq triggers 'during' hotplug.
@@ -8526,6 +8530,10 @@ static int should_we_balance(struct lb_env *env)
 	if (env->idle == CPU_NEWLY_IDLE)
 		return 1;
 
+	trace_printk("swb_tick: cpu=%i sd=%lx balance_mask=%lx\n",
+		     env->dst_cpu, *cpumask_bits(sched_domain_span(env->sd)),
+		     *cpumask_bits(group_balance_mask(sg)));
+
 	/* Try to find first idle cpu */
 	for_each_cpu_and(cpu, group_balance_mask(sg), env->cpus) {
 		if (!idle_cpu(cpu))
@@ -8535,8 +8543,16 @@ static int should_we_balance(struct lb_env *env)
 		break;
 	}
 
-	if (balance_cpu == -1)
+	trace_printk("swb_tick: cpu=%i sd=%lx first_idle=%i\n",
+		     env->dst_cpu, *cpumask_bits(sched_domain_span(env->sd)),
+		     balance_cpu);
+
+	if (balance_cpu == -1) {
 		balance_cpu = group_balance_cpu(sg);
+		trace_printk("swb_tick: cpu=%i sd=%lx balance=%i\n",
+			     env->dst_cpu, *cpumask_bits(sched_domain_span(env->sd)),
+			     balance_cpu);
+	}
 
 	/*
 	 * First idle cpu or the first cpu(busiest) in this sched group
