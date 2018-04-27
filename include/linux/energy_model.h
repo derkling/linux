911bf4a62dfd65c699a281c25e49a8a168cf6dac
Quentin Perret <quentin.perret@arm.com>
2018-04-27 11:19:25 +0100

    PM / EM: Add Energy Model manipulation methods
    NOT READY
    
    I'm still not sure if this should be part of the commit that introduces
    the energy model framework or the commit that introduces compute_energy()
    later in fair.c. Let's keep it separate for now ...

--- include/linux/energy_model.h
+++ include/linux/energy_model.h
@@ -4,6 +4,7 @@
 #include <linux/types.h>
 #include <linux/cpumask.h>
 #include <linux/jump_label.h>
+#include <linux/sched/cpufreq.h>
 
 #ifdef CONFIG_ENERGY_MODEL
 struct em_cap_state {
@@ -45,6 +46,33 @@ void em_normalize_cpu_capacity(void);
 #define freq_domain_span(fd) &((fd)->span)
 #define for_each_freq_domain(fd, em) \
 			list_for_each_entry(fd, &em->freq_domains, next)
+
+static inline struct em_cap_state *em_find_cap_state_ceil(int cpu,
+				unsigned long util, struct energy_model* em)
+{
+	struct em_cap_state_table *table = *per_cpu_ptr(em->cap_tables, cpu);
+	struct em_cap_state *cs = NULL;
+	unsigned long freq;
+	int i;
+
+	/* Map the utilization value to a frequency */
+	cs = &table->cap_states[table->nr_cap_states-1];
+	freq = map_util_freq(util, cs->freq, cs->cap);
+
+	/* Find the lowest capacity state above this frequency */
+	for (i = 0; i < table->nr_cap_states; i++) {
+		cs = &table->cap_states[i];
+		if (cs->freq >= freq)
+			break;
+	}
+
+	return cs;
+}
+
+static inline long em_get_cap_state_energy(struct em_cap_state *cs, long util)
+{
+	return cs->power * util / cs->cap;
+}
 #else
 
 struct em_freq_domain;
@@ -70,6 +98,16 @@ static inline int em_build_freq_domain(cpumask_t *span, int nr_states, long (*f)
 }
 
 static inline void em_normalize_cpu_capacity(void) { }
+
+static inline struct em_cap_state *em_find_cap_state_ceil(int cpu,
+				unsigned long util, struct energy_model* em)
+{
+	return NULL;
+}
+static inline long em_get_cap_state_energy(struct em_cap_state *cs, long util)
+{
+	return -EINVAL;
+}
 #endif
 
 #endif
