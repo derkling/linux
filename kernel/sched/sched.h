d3d48eca87f3e030e0adb747670dccbf4c5d4773
Vincent Guittot <vincent.guittot@linaro.org>
2018-05-25 15:12:23 +0200

    FROMLIST: sched/rt: add rt_rq utilization tracking
    schedutil governor relies on cfs_rq's util_avg to choose the OPP when cfs
    tasks are running. When the CPU is overloaded by cfs and rt tasks, cfs tasks
    are preempted by rt tasks and in this case util_avg reflects the remaining
    capacity but not what cfs want to use. In such case, schedutil can select a
    lower OPP whereas the CPU is overloaded. In order to have a more accurate
    view of the utilization of the CPU, we track the utilization that is
    "stolen" by rt tasks.
    
    rt_rq uses rq_clock_task and cfs_rq uses cfs_rq_clock_task but they are
    the same at the root group level, so the PELT windows of the util_sum are
    aligned.
    
    Change-Id: I2995bc9bedc4cdbdd6e490beddf771518f99cedd
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -593,6 +593,7 @@ struct rt_rq {
 	unsigned long		rt_nr_total;
 	int			overloaded;
 	struct plist_head	pushable_tasks;
+
 #endif /* CONFIG_SMP */
 	int			rt_queued;
 
@@ -857,6 +858,7 @@ struct rq {
 
 	u64			rt_avg;
 	u64			age_stamp;
+	struct sched_avg	avg_rt;
 	u64			idle_stamp;
 	u64			avg_idle;
 
@@ -2191,6 +2193,11 @@ static inline unsigned long cpu_util_cfs(struct rq *rq)
 	}
 
 	return util;
+}
+
+static inline unsigned long cpu_util_rt(struct rq *rq)
+{
+	return rq->avg_rt.util_avg;
 }
 #endif
 
