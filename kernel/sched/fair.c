35166e1ec8974093ff362bc5fb59a4cfc9729cb4
Chris Redpath <chris.redpath@arm.com>
2018-05-25 16:17:16 +0100

    ANDROID: sched/fair: Add missing max_capacity init to update_cpu_capacity
    Somehow we did not add the code which populates max_capacity when the
    rest of the feature was added. Rectify that here.
    
    Change-Id: Ice1f1b069f349b39f8801fa9ebdc9f0b482a1f29
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8797,6 +8797,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	cpu_rq(cpu)->cpu_capacity = capacity;
 	sdg->sgc->capacity = capacity;
 	sdg->sgc->min_capacity = capacity;
+	sdg->sgc->max_capacity = capacity;
 }
 
 void update_group_capacity(struct sched_domain *sd, int cpu)
