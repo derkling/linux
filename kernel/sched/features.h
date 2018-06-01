60901313af36056ff1fa14897b5354930dcad870
Ionela Voinescu <ionela.voinescu@arm.com>
2018-06-01 19:01:17 +0100

    sched/features: prefer idle through slow path
    Change-Id: Iabe3f5e7941332c47d55f1c37cc9b34057d811db
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -106,6 +106,6 @@ SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
  *   ON: If the target CPU saves any energy, use that.
  *   OFF: Use whichever of target or backup saves most.
  */
-SCHED_FEAT(EAS_PREFER_IDLE, true)
+SCHED_FEAT(EAS_PREFER_IDLE, false)
 SCHED_FEAT(FIND_BEST_TARGET, true)
 SCHED_FEAT(FBT_STRICT_ORDER, true)
