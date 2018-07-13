e847f75dfca28d4efaca958fa48b81f9dc013624
Patrick Bellasi <patrick.bellasi@arm.com>
2018-07-13 18:49:56 +0100

    DEBUG
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/pelt.c
+++ kernel/sched/pelt.c
@@ -308,7 +308,8 @@ int __update_load_avg_se(u64 now, int cpu, struct cfs_rq *cfs_rq, struct sched_e
 		if (entity_is_task(se)) {
 			trace_printk("update_util_se: pid=%d comm=%s cpu=%d "
 					"util_avg=%lu clamp_util_avg=%d "
-					"uclamp_min=%d uclamp_max=%d",
+					"uclamp_min=%d uclamp_max=%d "
+					"event=se_update",
 					task_of(se)->pid, task_of(se)->comm, cpu,
 					se->avg.util_avg,
 					uclamp_util(cpu_rq(cpu), se->avg.util_avg),
@@ -338,7 +339,8 @@ int __update_load_avg_cfs_rq(u64 now, int cpu, struct cfs_rq *cfs_rq)
 		if (&cpu_rq(cpu)->cfs == cfs_rq) {
 			trace_printk("update_util_rq: cpu=%d "
 					"util_avg=%lu clamp_util_avg=%d "
-					"uclamp_min=%d uclamp_max=%d",
+					"uclamp_min=%d uclamp_max=%d "
+					"event=rq_update",
 					cpu, cfs_rq->avg.util_avg,
 					uclamp_util(cpu_rq(cpu), cfs_rq->avg.util_avg),
 					uclamp_value(cpu_rq(cpu), UCLAMP_MIN),
