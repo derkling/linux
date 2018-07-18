44aa484800b3ce2bb1135b9005eff3f007c97b77
Quentin Perret <quentin.perret@arm.com>
2018-07-18 14:32:48 +0100

    sched/cpufreq: Refactor the utilization aggregation method
    Schedutil aggregates the PELT signals of CFS, RT, DL and IRQ in order
    to decide which frequency to request. Energy Aware Scheduling (EAS)
    needs to be able to predict those requests to assess the energy impact
    of scheduling decisions. However, the PELT signals aggregation is only
    done in schedutil for now, hence making it hard to synchronize it with
    EAS.
    
    To address this issue, introduce schedutil_freq_util() to perform the
    aforementioned aggregation and make it available to other parts of the
    scheduler. Since frequency selection and energy estimation still need
    to deal with RT and DL signals slightly differently, schedutil_freq_util()
    is called with a different 'type' parameter in those two contexts, and
    returns an aggregated utilization signal accordingly.
    
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2187,7 +2187,15 @@ static inline void cpufreq_update_util(struct rq *rq, unsigned int flags) {}
 # define arch_scale_freq_invariant()	false
 #endif
 
+enum schedutil_type {
+	frequency_util,
+	energy_util,
+};
+
 #ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
+unsigned long schedutil_freq_util(int cpu, unsigned long util_cfs,
+				  enum schedutil_type type);
+
 static inline unsigned long cpu_bw_dl(struct rq *rq)
 {
 	return (rq->dl.running_bw * SCHED_CAPACITY_SCALE) >> BW_SHIFT;
@@ -2213,6 +2221,13 @@ static inline unsigned long cpu_util_cfs(struct rq *rq)
 static inline unsigned long cpu_util_rt(struct rq *rq)
 {
 	return READ_ONCE(rq->avg_rt.util_avg);
+}
+
+#else /* CONFIG_CPU_FREQ_GOV_SCHEDUTIL */
+static inline unsigned long schedutil_freq_util(int cpu, unsigned long util,
+				  enum schedutil_type type)
+{
+	return util;
 }
 #endif
 
