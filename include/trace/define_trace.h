2bdbf223e4453aaccf09add5bbb85237f3a85d3b
Patrick Bellasi <patrick.bellasi@arm.com>
2018-10-16 13:53:47 +0100

    tracing: Fix DELCARE_TRACE_CONDITION() re-definitions
    The DECLARE_TRACE_CONDITION() macro is pretty much similar to
    DECLARE_TRACE() where an additional TP_CONDITION() parameter can be
    specified to support trace events filtering based on a condition to be
    checked only when the trace point is enabled.
    
    However, the current implementations of DECLARE_TRACE{_CONDITION}() are
    misaligned and the "_CONDITION" variant is missing some definitions and
    generates compile time warnings and errors for double symbols
    definitions.
    
    Fix that by adding the missing definitions. Do that by following the
    same template already used for the ! "_CONDITION()" variant.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Steven Rostedt <rostedt@goodmis.org> (maintainer:TRACING)
    Cc: Ingo Molnar <mingo@redhat.com> (maintainer:TRACING)
    Cc: linux-kernel@vger.kernel.org (open list)

--- include/trace/define_trace.h
+++ include/trace/define_trace.h
@@ -72,6 +72,10 @@
 #define DECLARE_TRACE(name, proto, args)	\
 	DEFINE_TRACE(name)
 
+#undef DECLARE_TRACE_CONDITION
+#define DECLARE_TRACE_CONDITION(name, proto, args, cond)	\
+	DEFINE_TRACE_CONDITION(name)
+
 #undef TRACE_INCLUDE
 #undef __TRACE_INCLUDE
 
@@ -98,6 +102,9 @@
 #undef DECLARE_TRACE
 #define DECLARE_TRACE(name, proto, args)
 
+#undef DECLARE_TRACE_CONDITION
+#define DECLARE_TRACE_CONDITION(name, proto, args, cond)
+
 #ifdef TRACEPOINTS_ENABLED
 #include <trace/trace_events.h>
 #include <trace/perf.h>
@@ -117,6 +124,7 @@
 #undef DEFINE_EVENT_CONDITION
 #undef TRACE_HEADER_MULTI_READ
 #undef DECLARE_TRACE
+#undef DECLARE_TRACE_CONDITION
 
 /* Only undef what we defined in this file */
 #ifdef UNDEF_TRACE_INCLUDE_FILE
