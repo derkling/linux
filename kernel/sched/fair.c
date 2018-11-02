9fdd0c4082a9357bb8f180e6bfedca306b115c59
Patrick Bellasi <patrick.bellasi@arm.com>
2018-11-02 15:41:21 +0000

    sched/fair: util_est: use {add,sub}_positive
    In fair.c we already have {add,sub}_positive calls which ensure clamped
    overflow operatons on asynchronously observable variables.
    
    Let's use these calls to reduce codelines and make the caller path a bit
    simpler to read. This will end up adding one additional READ_ONCE, but
    it has been measured not having a noticiable overhead.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: linux-kernel@vger.kernel.org

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3626,15 +3626,11 @@ static inline unsigned long task_util_est(struct task_struct *p)
 static inline void util_est_enqueue(struct cfs_rq *cfs_rq,
 				    struct task_struct *p)
 {
-	unsigned int enqueued;
-
 	if (!sched_feat(UTIL_EST))
 		return;
 
 	/* Update root cfs_rq's estimated utilization */
-	enqueued  = cfs_rq->avg.util_est.enqueued;
-	enqueued += _task_util_est(p);
-	WRITE_ONCE(cfs_rq->avg.util_est.enqueued, enqueued);
+	add_positive(&cfs_rq->avg.util_est.enqueued, _task_util_est(p));
 }
 
 /*
@@ -3660,9 +3656,7 @@ util_est_dequeue(struct cfs_rq *cfs_rq, struct task_struct *p, bool task_sleep)
 		return;
 
 	/* Update root cfs_rq's estimated utilization */
-	ue.enqueued  = cfs_rq->avg.util_est.enqueued;
-	ue.enqueued -= min_t(unsigned int, ue.enqueued, _task_util_est(p));
-	WRITE_ONCE(cfs_rq->avg.util_est.enqueued, ue.enqueued);
+	sub_positive(&cfs_rq->avg.util_est.enqueued, _task_util_est(p));
 
 	/*
 	 * Skip update of task's estimated utilization when the task has not
