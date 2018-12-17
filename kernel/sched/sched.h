f926a17dc611dc4385f84e9312d5dacd63f8c106
Patrick Bellasi <patrick.bellasi@arm.com>
2018-12-17 12:30:47 +0000

    sched/fair: uclamp: Add uclamp support to energy_compute()
    The Energy Aware Scheduler (AES) estimates the energy impact of waking
    up a task on a given CPU. This estimation is based on:
     a) an (active) power consumptions defined for each CPU frequency
     b) an estimation of which frequency will be used on each CPU
     c) an estimation of the busy time (utilization) of each CPU
    
    Utilization clamping can affect both b) and c) estimations. A CPU is
    expected to run:
     - on an higher than required frequency, but for a shorter time, in case
       its estimated utilization will be smaller then the minimum utilization
       enforced by uclamp
     - on a smaller than required frequency, but for a longer time, in case
       its estimated utilization is bigger then the maximum utilization
       enforced by uclamp
    
    While effects on busy time for both boosted/capped tasks are already
    considered by compute_energy(), clamping effects on frequency selection
    are currently ignored by that function.
    
    Fix it by considering how CPU clamp values will be affected by a
    task waking up and being RUNNABLE on that CPU.
    
    Do that by refactoring schedutil_freq_util() to take an additional
    task_struct* which allows EAS to evaluate the impact on clamp values of
    a task being eventually queued in a CPU. Clamp values are applied to the
    RT+CFS utilization only when a FREQUENCY_UTIL is required by
    compute_energy().
    
    Do note that switching from ENERGY_UTIL to FREQUENCY_UTIL in the
    computation of cpu_util signal implies that we are more likely to
    estimate the higherst OPP when a RT task is running in another CPU of
    the same performance domain. This can have an impact on energy
    estimation but:
     - it's not easy to say which approach is better, since it quite likely
       depends on the use case
     - the original approach could still be obtained by setting a smaller
       task-specific util_min whenever required
    
    Since we are at that:
     - rename schedutil_freq_util() into schedutil_cpu_util(),
       since it's not only used for frequency selection.
     - use "unsigned int" instead of "unsigned long" whenever the tracked
       utilization value is not expected to overflow 32bit.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    ---
    Changes in v7:
     Message-ID: <20190122151404.5rtosic6puixado3@queper01-lin>
     - add a note on side-effects due to the usage of FREQUENCY_UTIL for
       performance domain frequency estimation
     - add a similer note to this changelog

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2336,6 +2336,7 @@ static inline unsigned long capacity_orig_of(int cpu)
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
+
 /**
  * enum schedutil_type - CPU utilization type
  * @FREQUENCY_UTIL:	Utilization used to select frequency
@@ -2351,15 +2352,9 @@ enum schedutil_type {
 	ENERGY_UTIL,
 };
 
-unsigned long schedutil_freq_util(int cpu, unsigned long util_cfs,
-				  unsigned long max, enum schedutil_type type);
-
-static inline unsigned long schedutil_energy_util(int cpu, unsigned long cfs)
-{
-	unsigned long max = arch_scale_cpu_capacity(NULL, cpu);
-
-	return schedutil_freq_util(cpu, cfs, max, ENERGY_UTIL);
-}
+unsigned int schedutil_cpu_util(int cpu, unsigned int util_cfs,
+				unsigned int max, enum schedutil_type type,
+				struct task_struct *p);
 
 static inline unsigned long cpu_bw_dl(struct rq *rq)
 {
@@ -2388,10 +2383,7 @@ static inline unsigned long cpu_util_rt(struct rq *rq)
 	return READ_ONCE(rq->avg_rt.util_avg);
 }
 #else /* CONFIG_CPU_FREQ_GOV_SCHEDUTIL */
-static inline unsigned long schedutil_energy_util(int cpu, unsigned long cfs)
-{
-	return cfs;
-}
+#define schedutil_cpu_util(cpu, util_cfs, max, type, p) 0
 #endif
 
 #ifdef CONFIG_HAVE_SCHED_AVG_IRQ
