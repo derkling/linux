1267f2c05d720cdb566e562f597929135ab83045
Patrick Bellasi <patrick.bellasi@arm.com>
2019-04-18 15:52:58 +0100

    TESTING: Force SELinux permissive mode by default
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -105,20 +105,8 @@ struct selinux_state selinux_state;
 /* SECMARK reference count */
 static atomic_t selinux_secmark_refcount = ATOMIC_INIT(0);
 
-#ifdef CONFIG_SECURITY_SELINUX_DEVELOP
-static int selinux_enforcing_boot;
-
-static int __init enforcing_setup(char *str)
-{
-	unsigned long enforcing;
-	if (!kstrtoul(str, 0, &enforcing))
-		selinux_enforcing_boot = enforcing ? 1 : 0;
-	return 1;
-}
-__setup("enforcing=", enforcing_setup);
-#else
-#define selinux_enforcing_boot 1
-#endif
+// HACK: keep it off by default
+#define selinux_enforcing_boot 0
 
 #ifdef CONFIG_SECURITY_SELINUX_BOOTPARAM
 int selinux_enabled = CONFIG_SECURITY_SELINUX_BOOTPARAM_VALUE;
