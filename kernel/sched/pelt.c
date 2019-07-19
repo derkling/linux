68cd6a7f72661d535067729706b6873410108449
Patrick Bellasi <patrick.bellasi@arm.com>
2019-07-19 18:57:21 +0100

    Trying to update blocked task utilization
    This is the first attempt... we still to set call sites and provide the
    update for the CFS RQs.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/pelt.c
+++ kernel/sched/pelt.c
@@ -245,6 +245,7 @@ pelt_update_avg(struct pelt_attrs *pa, enum pelt_flavour flavour,
 	u32 divider;
 
 	if (flavour == PELT_ALL) {
+#ifdef CONFIG_SMP
 		divider = PELT_MAX(PELT_LOAD) - 1024 + pa->period_contrib;
 
 		ps = &pelt_all_of(pa)->load;
@@ -252,6 +253,7 @@ pelt_update_avg(struct pelt_attrs *pa, enum pelt_flavour flavour,
 
 		ps = &pelt_all_of(pa)->runnable;
 		ps->avg = div_u64(runnable * ps->sum, divider);
+#endif /* CONFIG_SMP */
 
 		divider = PELT_MAX(PELT_UTIL) - 1024 + pa->period_contrib;
 
@@ -260,6 +262,7 @@ pelt_update_avg(struct pelt_attrs *pa, enum pelt_flavour flavour,
 	}
 
 	if (flavour == PELT_LOAD) {
+#ifdef CONFIG_SMP
 		divider = PELT_MAX(PELT_LOAD) - 1024 + pa->period_contrib;
 
 		ps = &pelt_load_of(pa)->load;
@@ -267,7 +270,9 @@ pelt_update_avg(struct pelt_attrs *pa, enum pelt_flavour flavour,
 
 		ps = &pelt_load_of(pa)->runnable;
 		ps->avg = div_u64(runnable * ps->sum, divider);
+#endif /* CONFIG_SMP */
 	}
+	
 
 	if (flavour == PELT_UTIL) {
 		divider = PELT_MAX(PELT_UTIL) - 1024 + pa->period_contrib;
@@ -278,9 +283,51 @@ pelt_update_avg(struct pelt_attrs *pa, enum pelt_flavour flavour,
 }
 
 
-
-int __update_load_avg_blocked_se(u64 now, struct sched_entity *se)
+int pelt_update_task_util_blocked(struct task_struct *p,
+				  struct cfs_rq *prev, struct cfs_rq *next)
 {
+	u64 p_last_update_time;
+	u64 n_last_update_time;
+
+	/*
+	 * We are supposed to update the task to "current" time, then its up to
+	 * date and ready to go to new CPU/cfs_rq. But we have difficulty in
+	 * getting what current time is, so simply throw away the out-of-date
+	 * time. This will result in the wakee task is less decayed, but giving
+	 * the wakee more load sounds not bad.
+	 */
+	if (!(p->pelt.attrs.last_update_time && prev))
+		return;
+
+#ifndef CONFIG_64BIT
+	{
+		u64 p_last_update_time_copy;
+		u64 n_last_update_time_copy;
+
+		do {
+			p_last_update_time_copy = prev->load_last_update_time_copy;
+			n_last_update_time_copy = next->load_last_update_time_copy;
+
+			smp_rmb();
+
+			p_last_update_time = prev->avg.last_update_time;
+			n_last_update_time = next->avg.last_update_time;
+
+		} while (p_last_update_time != p_last_update_time_copy ||
+			 n_last_update_time != n_last_update_time_copy);
+	}
+#else
+	p_last_update_time = prev->avg.last_update_time;
+	n_last_update_time = next->avg.last_update_time;
+#endif
+
+	if (pelt_update_sum(p_last_update_time, &p->pelt.attrs,
+			    PELT_UTIL, 0, 0, 0) {
+		pelt_update_avg(&p->pelt.attrs, 0, 0);
+		trace_pelt_se_tp(se);
+	}
+
+	p->pelt.attrs.last_update_time = n_last_update_time;
 }
 
 int __update_load_avg_se(u64 now, struct cfs_rq *cfs_rq, struct sched_entity *se)
