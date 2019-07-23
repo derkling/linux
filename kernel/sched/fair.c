25aa0c2f94751a42d6a6fde58c8dd73098665089
Patrick Bellasi <patrick.bellasi@arm.com>
2019-07-23 15:33:31 +0100

    sched/fair: util_est: Add new tracepoints for task and cfs_rq
    Each time a task is enqueued/dequeued to/from a rq, the rq estimated utilization
    is updated. The estimated utilization of a task is updated instead each time
    a task completes an activation.
    
    Add a new pair of tracepoints which allows to have modules defining trace
    events to collect utilization estimation updates.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3721,6 +3721,8 @@ static inline void util_est_enqueue(struct cfs_rq *cfs_rq,
 	enqueued  = cfs_rq->avg.util_est.enqueued;
 	enqueued += _task_util_est(p);
 	WRITE_ONCE(cfs_rq->avg.util_est.enqueued, enqueued);
+	trace_util_est_task_tp(p);
+	trace_util_est_cfs_tp(rq_of(cfs_rq));
 }
 
 /*
@@ -3750,6 +3752,7 @@ util_est_dequeue(struct cfs_rq *cfs_rq, struct task_struct *p, bool task_sleep)
 	ue.enqueued  = cfs_rq->avg.util_est.enqueued;
 	ue.enqueued -= min_t(unsigned int, ue.enqueued, _task_util_est(p));
 	WRITE_ONCE(cfs_rq->avg.util_est.enqueued, ue.enqueued);
+	trace_util_est_cfs_tp(rq_of(cfs_rq));
 
 	/*
 	 * Skip update of task's estimated utilization when the task has not
@@ -3804,6 +3807,7 @@ util_est_dequeue(struct cfs_rq *cfs_rq, struct task_struct *p, bool task_sleep)
 	ue.ewma  += last_ewma_diff;
 	ue.ewma >>= UTIL_EST_WEIGHT_SHIFT;
 	WRITE_ONCE(p->se.avg.util_est, ue);
+	trace_util_est_task_tp(p);
 }
 
 static inline int task_fits_capacity(struct task_struct *p, long capacity)
